2c01cd24ac30967edbf7c29d3f080c2bc14c100c
xzxhub0-source <xzxhub0@gmail.com>
2026-03-08 13:10:32 -0400

    Create xzx_antidetection.h

---
+++ injector/dylib/xzx_antidetection.h
@@ -0,0 +1,29 @@
+#ifndef XZX_ANTIDETECTION_H
+#define XZX_ANTIDETECTION_H
+
+#import <Foundation/Foundation.h>
+
+@interface XZXAntiDetection : NSObject
++ (instancetype)shared;
+- (void)initializeProtection;
+- (void)runIntegrityChecks;
+- (BOOL)isUnderInvestigation;
+- (void)emergencyShutdown;
+- (void)obfuscateMemory;
+- (void)randomizeInjectionPattern;
+- (void)spoofConnections;
+- (void)cleanTraces;
+- (void)bypassAdonis;
+- (void)bypassKRX;
+- (void)bypassSentinelAC;
+- (void)bypassPhysicsChecks;
+- (void)updateBanProbability;
+- (BOOL)shouldSelfDestruct;
+
+@property (nonatomic, assign) NSInteger currentStrikes;
+@property (nonatomic, assign) NSInteger maxStrikes;
+@property (nonatomic, strong) NSMutableDictionary *strikeReasons;
+@property (nonatomic, assign) double banProbability;
+@end
+
+#endif

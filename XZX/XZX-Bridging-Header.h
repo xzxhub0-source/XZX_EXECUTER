3d81cf61fdc211012edc54969b8d783c016c06fd
xzxhub0-source <xzxhub0@gmail.com>
2026-03-08 18:02:31 -0400

    Create XZX-Bridging-Header.h

---
+++ XZX/XZX-Bridging-Header.h
@@ -0,0 +1,10 @@
+#ifndef XZX_Bridging_Header_h
+#define XZX_Bridging_Header_h
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+// Include Lua executor header
+#include "injector/dylib/Core/LuaExecutor.h"
+
+#endif

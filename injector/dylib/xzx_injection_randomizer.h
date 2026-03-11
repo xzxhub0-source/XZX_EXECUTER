f20a8709a891c342b331547b12e0770b2985bb72
xzxhub0-source <xzxhub0@gmail.com>
2026-03-10 22:20:24 -0400

    Update xzx_injection_randomizer.h

--- injector/dylib/xzx_injection_randomizer.h
+++ injector/dylib/xzx_injection_randomizer.h
@@ -1,22 +1,35 @@
-#ifndef XZX_INJECTION_RANDOMIZER_H
-#define XZX_INJECTION_RANDOMIZER_H
+#ifndef XZX_HOOK_MANAGER_H
+#define XZX_HOOK_MANAGER_H
 
 #import <Foundation/Foundation.h>
 
-@interface XZXInjectionRandomizer : NSObject
+@interface XZXHookManager : NSObject
 + (instancetype)shared;
-- (void)randomizeNextInjection;
-- (void)randomizeInjectionTiming;
-- (void)randomizeMemoryAllocation;
-- (void)randomizeThreadCreation;
-- (void)avoidSignaturePatterns;
-- (void)useAsynchronousInjection;
-- (void)useMemoryMappedInjection;
-- (double)randomnessScore;
-- (BOOL)isPatternDetectable;
+- (void)initializeHookSystem;
+- (void)addHook:(NSString *)functionName original:(void *)original hook:(void *)hook;
+- (void)removeHook:(NSString *)functionName;
+- (void)restoreAllFunctions;
+- (BOOL)areHooksExposed;
+- (void)hideFromAdonis;
+- (void)spoofConnectionList;
+- (void)randomizeHookOrder;
+- (double)exposureRatio;
+- (void *)cloneFunction:(void *)original;
+- (void)restoreFunction:(NSString *)functionName;
+- (NSString *)getFunctionHash:(void *)function;
+- (NSArray *)getConnections:(NSString *)signal;
+- (void)fireSignal:(NSString *)signal withArguments:(NSArray *)args;
+- (NSArray *)getSignalArguments:(NSString *)signal;
+- (BOOL)canSignalReplicate:(NSString *)signal;
+- (void)replicateSignal:(NSString *)signal withArguments:(NSArray *)args;
+- (void)obfuscateHookPointers;
+- (void)rotateHookPatterns;
+- (void)simulateNormalHooks;
 
-@property (nonatomic, assign) NSInteger lastInjectionMethod;
-@property (nonatomic, strong) NSMutableArray *injectionHistory;
+@property (nonatomic, strong) NSMutableDictionary *hooks;
+@property (nonatomic, strong) NSMutableDictionary *originalFunctions;
+@property (nonatomic, strong) NSMutableArray *exposedHooks;
+@property (nonatomic, strong) NSMutableDictionary *hookHistory;
 @end
 
 #endif

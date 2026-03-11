de8424fee6be99664f8fd0dd696813b9b668eef1
xzxhub0-source <xzxhub0@gmail.com>
2026-03-10 22:23:15 -0400

    Update xzx_injection_randomizer.h

--- injector/dylib/xzx_injection_randomizer.h
+++ injector/dylib/xzx_injection_randomizer.h
@@ -1,35 +1,27 @@
-#ifndef XZX_HOOK_MANAGER_H
-#define XZX_HOOK_MANAGER_H
+#ifndef XZX_INJECTION_RANDOMIZER_H
+#define XZX_INJECTION_RANDOMIZER_H
 
 #import <Foundation/Foundation.h>
 
-@interface XZXHookManager : NSObject
+@interface XZXInjectionRandomizer : NSObject
 + (instancetype)shared;
-- (void)initializeHookSystem;
-- (void)addHook:(NSString *)functionName original:(void *)original hook:(void *)hook;
-- (void)removeHook:(NSString *)functionName;
-- (void)restoreAllFunctions;
-- (BOOL)areHooksExposed;
-- (void)hideFromAdonis;
-- (void)spoofConnectionList;
-- (void)randomizeHookOrder;
-- (double)exposureRatio;
-- (void *)cloneFunction:(void *)original;
-- (void)restoreFunction:(NSString *)functionName;
-- (NSString *)getFunctionHash:(void *)function;
-- (NSArray *)getConnections:(NSString *)signal;
-- (void)fireSignal:(NSString *)signal withArguments:(NSArray *)args;
-- (NSArray *)getSignalArguments:(NSString *)signal;
-- (BOOL)canSignalReplicate:(NSString *)signal;
-- (void)replicateSignal:(NSString *)signal withArguments:(NSArray *)args;
-- (void)obfuscateHookPointers;
-- (void)rotateHookPatterns;
-- (void)simulateNormalHooks;
+- (void)randomizeNextInjection;
+- (void)randomizeInjectionTiming;
+- (void)randomizeMemoryAllocation;
+- (void)randomizeThreadCreation;
+- (void)avoidSignaturePatterns;
+- (void)useAsynchronousInjection;
+- (void)useMemoryMappedInjection;
+- (void)useDynamicCodeGeneration;
+- (void)obfuscateInjectionPoint;
+- (double)randomnessScore;
+- (BOOL)isPatternDetectable;
+- (void)rotateInjectionMethod;
+- (void)simulateLegitimateBehavior;
 
-@property (nonatomic, strong) NSMutableDictionary *hooks;
-@property (nonatomic, strong) NSMutableDictionary *originalFunctions;
-@property (nonatomic, strong) NSMutableArray *exposedHooks;
-@property (nonatomic, strong) NSMutableDictionary *hookHistory;
+@property (nonatomic, assign) NSInteger lastInjectionMethod;
+@property (nonatomic, strong) NSMutableArray *injectionHistory;
+@property (nonatomic, strong) NSMutableDictionary *methodPatterns;
 @end
 
 #endif

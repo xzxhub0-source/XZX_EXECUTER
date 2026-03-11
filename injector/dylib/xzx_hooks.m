270d6bb41947648c9fb1c46a6e9bcb143b6568ef
xzxhub0-source <xzxhub0@gmail.com>
2026-03-10 20:05:36 -0400

    Update xzx_hooks.m

--- injector/dylib/xzx_hooks.m
+++ injector/dylib/xzx_hooks.m
@@ -1,26 +1,29 @@
 #import "xzx_hooks.h"
+#import "xzx_core.h"
 #import <mach/mach.h>
 #import <mach-o/dyld.h>
-#import <sys/mman.h>
-#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+#import <objc/message.h>
 
-static void *original_functions[256];
+static void* original_functions[256];
 static int hook_count = 0;
-static uintptr_t roblox_base = 0;
 
-#ifndef PAGE_SIZE
-#define PAGE_SIZE 16384
-#endif
+typedef void (*TaskSchedulerHook)(void*, void*);
+static TaskSchedulerHook original_task_scheduler = NULL;
 
-static uint32_t hook_trampoline[] = {
-    0x58000050,
-    0xD61F0200,
-    0x00000000,
-    0x00000000
-};
+void hook_task_scheduler(void* scheduler, void* task) {
+    notify_game_joined();
+    if (original_task_scheduler) {
+        original_task_scheduler(scheduler, task);
+    }
+}
 
-void install_hook(void *target, void *replacement) {
+void install_hook(void* target, void* replacement, void** original) {
     if (!target || !replacement) return;
+    
+    if (original) {
+        *original = target;
+    }
     
     original_functions[hook_count++] = target;
     
@@ -28,10 +31,10 @@ void install_hook(void *target, void *replacement) {
     vm_protect(mach_task_self(), address & ~(PAGE_SIZE - 1), 
                PAGE_SIZE, 0, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_EXECUTE);
     
-    memcpy(target, hook_trampoline, sizeof(hook_trampoline));
+    uint32_t branch_instruction = 0x14000000 | 
+        (((uint32_t)((uintptr_t)replacement - (uintptr_t)target) >> 2) & 0x03FFFFFF);
     
-    uint64_t *addr_ptr = (uint64_t *)((uint8_t *)target + 8);
-    *addr_ptr = (uint64_t)replacement;
+    memcpy(target, &branch_instruction, 4);
     
     vm_protect(mach_task_self(), address & ~(PAGE_SIZE - 1), 
                PAGE_SIZE, 0, VM_PROT_READ | VM_PROT_EXECUTE);
@@ -39,30 +42,36 @@ void install_hook(void *target, void *replacement) {
 
 void hook_roblox_functions(void) {
     uint32_t count = _dyld_image_count();
+    uintptr_t roblox_base = 0;
     
     for (uint32_t i = 0; i < count; i++) {
-        const char *name = _dyld_get_image_name(i);
-        if (strstr(name, "Roblox")) {
+        const char* name = _dyld_get_image_name(i);
+        if (strstr(name, "Roblox") || strstr(name, "RobloxPlayer")) {
             roblox_base = (uintptr_t)_dyld_get_image_header(i);
             break;
         }
     }
+    
+    if (!roblox_base) return;
+    
+    uintptr_t task_scheduler_addr = roblox_base + 0x12345678;
+    install_hook((void*)task_scheduler_addr, hook_task_scheduler, 
+                 (void**)&original_task_scheduler);
 }
 
 void unhook_roblox_functions(void) {
-    hook_count = 0;
-}
-
-bool isPlayerInGame(void) {
-    static int checkCounter = 0;
-    checkCounter++;
-    
-    if (checkCounter > 3) {
-        return YES;
+    for (int i = 0; i < hook_count; i++) {
+        void* target = original_functions[i];
+        vm_address_t address = (vm_address_t)target;
+        vm_protect(mach_task_self(), address & ~(PAGE_SIZE - 1), 
+                   PAGE_SIZE, 0, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_EXECUTE);
+        
+        uint32_t nop = 0xD503201F;
+        memcpy(target, &nop, 4);
+        
+        vm_protect(mach_task_self(), address & ~(PAGE_SIZE - 1), 
+                   PAGE_SIZE, 0, VM_PROT_READ | VM_PROT_EXECUTE);
     }
-    return NO;
-}
-
-uintptr_t get_datamodel_address(void) {
-    return 0;
+    
+    hook_count = 0;
 }

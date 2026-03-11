be7a378628f7cd6d232920c0ef27e74ddd63378c
xzxhub0-source <xzxhub0@gmail.com>
2026-03-10 22:17:55 -0400

    Update xzx_injection_randomizer.m

--- injector/dylib/xzx_injection_randomizer.m
+++ injector/dylib/xzx_injection_randomizer.m
@@ -1,6 +1,11 @@
 #import "xzx_injection_randomizer.h"
+#import <mach/mach.h>
+#import <mach-o/dyld.h>
+#import <sys/mman.h>
 
 static XZXInjectionRandomizer *sharedInjectionRandomizerInstance = nil;
+static int injection_methods[] = {0, 1, 2, 3, 4};
+static int current_method_index = 0;
 
 @implementation XZXInjectionRandomizer
 
@@ -16,44 +21,213 @@ static XZXInjectionRandomizer *sharedInjectionRandomizerInstance = nil;
     self = [super init];
     if (self) {
         _injectionHistory = [NSMutableArray array];
+        _methodPatterns = [NSMutableDictionary dictionary];
+        _lastInjectionMethod = -1;
+        [self initializeMethodPatterns];
     }
     return self;
 }
 
+- (void)initializeMethodPatterns {
+    _methodPatterns[@0] = @{
+        @"name": @"DirectHook",
+        @"signature": @[@"0x90", @"0x90", @"0x90"],
+        @"detection_rate": @0.3
+    };
+    _methodPatterns[@1] = @{
+        @"name": @"Trampoline",
+        @"signature": @[@"0xE9", @"0x??", @"0x??"],
+        @"detection_rate": @0.5
+    };
+    _methodPatterns[@2] = @{
+        @"name": @"VTableSwap",
+        @"signature": @[@"0x48", @"0x8B", @"0x01"],
+        @"detection_rate": @0.4
+    };
+    _methodPatterns[@3] = @{
+        @"name": @"MemoryMap",
+        @"signature": @[@"0x??", @"0x??", @"0x??"],
+        @"detection_rate": @0.2
+    };
+    _methodPatterns[@4] = @{
+        @"name": @"DynamicCode",
+        @"signature": @[@"0x??", @"0x??", @"0x??"],
+        @"detection_rate": @0.1
+    };
+}
+
 - (void)randomizeNextInjection {
-    // Implementation
+    int attempts = 0;
+    int maxAttempts = 10;
+    int selectedMethod = -1;
+    
+    while (attempts < maxAttempts && selectedMethod == -1) {
+        int candidate = injection_methods[arc4random_uniform(sizeof(injection_methods) / sizeof(int))];
+        
+        NSDictionary *pattern = _methodPatterns[@(candidate)];
+        double detectionRate = [pattern[@"detection_rate"] doubleValue];
+        
+        if (detectionRate < 0.5 || arc4random_uniform(100) > detectionRate * 100) {
+            if (_injectionHistory.count > 0) {
+                NSNumber *lastMethod = [_injectionHistory lastObject];
+                if ([lastMethod intValue] != candidate) {
+                    selectedMethod = candidate;
+                }
+            } else {
+                selectedMethod = candidate;
+            }
+        }
+        
+        attempts++;
+    }
+    
+    if (selectedMethod == -1) {
+        selectedMethod = injection_methods[arc4random_uniform(sizeof(injection_methods) / sizeof(int))];
+    }
+    
+    _lastInjectionMethod = selectedMethod;
+    [_injectionHistory addObject:@(selectedMethod)];
+    
+    if (_injectionHistory.count > 10) {
+        [_injectionHistory removeObjectAtIndex:0];
+    }
 }
 
 - (void)randomizeInjectionTiming {
-    // Implementation
+    int baseDelay = 100;
+    int randomDelay = arc4random_uniform(500);
+    int totalDelay = baseDelay + randomDelay;
+    
+    usleep(totalDelay * 1000);
 }
 
 - (void)randomizeMemoryAllocation {
-    // Implementation
+    size_t baseSize = 1024;
+    size_t randomSize = arc4random_uniform(4096);
+    size_t totalSize = baseSize + randomSize;
+    
+    void *mem = mmap(NULL, totalSize, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    if (mem != MAP_FAILED) {
+        memset(mem, arc4random_uniform(256), totalSize);
+        munmap(mem, totalSize);
+    }
 }
 
 - (void)randomizeThreadCreation {
-    // Implementation
+    dispatch_queue_t queues[] = {
+        dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0),
+        dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
+        dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0),
+        dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)
+    };
+    
+    int queueIndex = arc4random_uniform(sizeof(queues) / sizeof(dispatch_queue_t));
+    dispatch_queue_t selectedQueue = queues[queueIndex];
+    
+    dispatch_async(selectedQueue, ^{
+        [NSThread sleepForTimeInterval:0.01 * arc4random_uniform(100)];
+    });
 }
 
 - (void)avoidSignaturePatterns {
-    // Implementation
+    NSDictionary *lastPattern = _methodPatterns[@(_lastInjectionMethod)];
+    NSArray *signature = lastPattern[@"signature"];
+    
+    if (_injectionHistory.count > 5) {
+        NSMutableArray *recentPatterns = [NSMutableArray array];
+        for (int i = (int)_injectionHistory.count - 5; i < _injectionHistory.count; i++) {
+            NSNumber *method = _injectionHistory[i];
+            [recentPatterns addObject:_methodPatterns[method][@"name"]];
+        }
+        
+        NSCountedSet *patternSet = [NSCountedSet setWithArray:recentPatterns];
+        for (NSString *pattern in patternSet) {
+            if ([patternSet countForObject:pattern] > 3) {
+                [self rotateInjectionMethod];
+                break;
+            }
+        }
+    }
 }
 
 - (void)useAsynchronousInjection {
-    // Implementation
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [self randomizeMemoryAllocation];
+    });
 }
 
 - (void)useMemoryMappedInjection {
-    // Implementation
+    NSString *tempFile = [NSTemporaryDirectory() stringByAppendingPathComponent:[NSString stringWithFormat:@"xzx_%08x", arc4random()]];
+    
+    int fd = open([tempFile UTF8String], O_RDWR | O_CREAT, 0666);
+    if (fd != -1) {
+        size_t size = 4096;
+        ftruncate(fd, size);
+        
+        void *map = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+        if (map != MAP_FAILED) {
+            memset(map, 0, size);
+            munmap(map, size);
+        }
+        
+        close(fd);
+        unlink([tempFile UTF8String]);
+    }
+}
+
+- (void)useDynamicCodeGeneration {
+    uint8_t *code = malloc(32);
+    if (code) {
+        code[0] = 0xC3;
+        
+        void *exec = mmap(NULL, 32, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+        if (exec != MAP_FAILED) {
+            memcpy(exec, code, 32);
+            __builtin___clear_cache(exec, (char *)exec + 32);
+            
+            void (*func)(void) = exec;
+            func();
+            
+            munmap(exec, 32);
+        }
+        
+        free(code);
+    }
+}
+
+- (void)obfuscateInjectionPoint {
+    uintptr_t *addr = (uintptr_t *)arc4random_uniform(0x100000000);
+    vm_address_t address = (vm_address_t)addr;
+    
+    vm_protect(mach_task_self(), address & ~(PAGE_SIZE - 1), PAGE_SIZE, 0,
+               VM_PROT_READ | VM_PROT_WRITE | VM_PROT_EXECUTE);
+    
+    uint32_t nop = 0x90;
+    for (int i = 0; i < 16; i++) {
+        memcpy((void *)(address + i), &nop, 1);
+    }
+    
+    vm_protect(mach_task_self(), address & ~(PAGE_SIZE - 1), PAGE_SIZE, 0,
+               VM_PROT_READ | VM_PROT_EXECUTE);
 }
 
 - (double)randomnessScore {
-    return 1.0;
+    if (_injectionHistory.count < 5) return 0.0;
+    
+    NSMutableSet *uniqueMethods = [NSMutableSet setWithArray:_injectionHistory];
+    double uniqueness = (double)uniqueMethods.count / _injectionHistory.count;
+    
+    return uniqueness;
 }
 
 - (BOOL)isPatternDetectable {
-    return NO;
+    if (_injectionHistory.count < 5) return NO;
+    
+    NSArray *lastFive = [_injectionHistory subarrayWithRange:NSMakeRange(_injectionHistory.count - 5, 5)];
+    NSSet *unique = [NSSet setWithArray:lastFive];
+    
+    return unique.count < 3;
 }
 
-@end
+- (void)rotateInjectionMethod {
+    current_method_index = (current_method_index + 1) % (sizeof(injection_method

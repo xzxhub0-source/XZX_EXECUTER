42412c9e37c31303bfefee918668d9e2c09e8dfe
xzxhub0-source <xzxhub0@gmail.com>
2026-03-12 06:11:42 -0400

    Update xzx_hooks.h

--- injector/dylib/xzx_hooks.h
+++ injector/dylib/xzx_hooks.h
@@ -4,12 +4,9 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <mach/mach.h>
 
 void hook_roblox_functions(void);
 void unhook_roblox_functions(void);
-void install_hook(void *target, void *replacement);
 bool isPlayerInGame(void);
-uintptr_t get_datamodel_address(void);
 
 #endif

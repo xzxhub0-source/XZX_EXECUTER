0eebe8dc10aaa8ededd51dd6f14be24fef4ff79c
xzxhub0-source <xzxhub0@gmail.com>
2026-03-12 05:57:32 -0400

    Update xzx_core.m

--- injector/dylib/xzx_core.m
+++ injector/dylib/xzx_core.m
@@ -1,11 +1,6 @@
 #import "xzx_core.h"
-#import "xzx_antidetection.h"
-#import "xzx_ban_protection.h"
-#import "xzx_injection_randomizer.h"
-#import "xzx_hook_manager.h"
 #import "Core/LuaExecutor.h"
 #import <UIKit/UIKit.h>
-#import <objc/runtime.h>
 
 static XZXCore *sharedCoreInstance = nil;
 
@@ -24,31 +19,23 @@ static XZXCore *sharedCoreInstance = nil;
     if (self) {
         _isInGame = NO;
         _editorWindow = nil;
+        NSLog(@"[XZX] Core instance created");
     }
     return self;
 }
 
-// REMOVED +load method completely - no automatic initialization
-
 - (void)initialize {
-    NSLog(@"[XZX] Manual initialize called");
+    NSLog(@"[XZX] initialize called");
     
     @try {
-        // Initialize Lua first (most basic)
         InitLua();
         NSLog(@"[XZX] Lua initialized");
         
-        // Initialize protection systems
-        [[XZXAntiDetection shared] initializeProtection];
-        [[XZXBanProtection shared] protectAccount];
-        [[XZXInjectionRandomizer shared] randomizeNextInjection];
-        [[XZXHookManager shared] initializeHookSystem];
-        
         [self startGameStateMonitoring];
-        NSLog(@"[XZX] All systems initialized");
+        NSLog(@"[XZX] Monitoring started");
         
     } @catch (NSException *exception) {
-        NSLog(@"[XZX] INIT ERROR: %@", exception);
+        NSLog(@"[XZX] CRASH: %@", exception);
     }
 }
 
@@ -58,41 +45,39 @@ static XZXCore *sharedCoreInstance = nil;
             BOOL lastInGame = NO;
             while (YES) {
                 @try {
-                    BOOL currentlyInGame = [self checkIfInGame];
+                    [NSThread sleepForTimeInterval:1.0];
+                    BOOL currentlyInGame = YES;
+                    
                     if (currentlyInGame && !lastInGame) {
                         dispatch_async(dispatch_get_main_queue(), ^{
                             [self onGameJoined];
-                        });
-                    } else if (!currentlyInGame && lastInGame) {
-                        dispatch_async(dispatch_get_main_queue(), ^{
-                            [self onGameLeft];
                         });
                     }
                     lastInGame = currentlyInGame;
                 } @catch (NSException *e) {
                     NSLog(@"[XZX] Monitor error: %@", e);
                 }
-                [NSThread sleepForTimeInterval:1.0];
             }
         }
     });
 }
 
-- (BOOL)checkIfInGame {
-    return YES;
-}
-
 - (void)onGameJoined {
-    if (self.isInGame) return;
     self.isInGame = YES;
     NSLog(@"[XZX] Game joined");
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"GameJoined" object:nil];
 }
 
 - (void)onGameLeft {
     self.isInGame = NO;
     NSLog(@"[XZX] Game left");
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"GameLeft" object:nil];
+}
+
+void notify_game_joined(void) {
+    [[XZXCore shared] onGameJoined];
+}
+
+void notify_game_left(void) {
+    [[XZXCore shared] onGameLeft];
 }
 
 @end

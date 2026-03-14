b638c917df5251e1a628080e08338300bff557cf
xzxhub0-source <xzxhub0@gmail.com>
2026-03-14 13:58:16 -0400

    Update xzx_ban_protection.h

--- injector/dylib/xzx_ban_protection.h
+++ injector/dylib/xzx_ban_protection.h
@@ -15,17 +15,10 @@
 - (void)clearRobloxCache;
 - (void)deleteLogs;
 - (void)removeTraces;
-- (void)updateBanPatterns;
-- (void)simulateHumanBehavior;
-- (void)randomizeActionTiming;
-- (void)avoidPatternRecognition;
 
 @property (nonatomic, assign) BOOL isBanned;
 @property (nonatomic, strong) NSString *currentHWID;
 @property (nonatomic, strong) NSMutableArray *accountPool;
-@property (nonatomic, strong) NSMutableArray *actionHistory;
-@property (nonatomic, assign) NSInteger actionCount;
-@property (nonatomic, assign) NSTimeInterval lastActionTime;
 @end
 
 #endif

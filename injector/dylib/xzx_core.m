926dd5501d72598b585439268c69bfdd9e16afd8
xzxhub0-source <xzxhub0@gmail.com>
2026-03-14 13:40:55 -0400

    Update xzx_core.m

--- injector/dylib/xzx_core.m
+++ injector/dylib/xzx_core.m
@@ -1,4 +1,7 @@
 #import "xzx_core.h"
+#import "XZX-Swift.h"
+#import "xzx_hooks.h"
+#import "xzx_antidetection.h"
 #import "Core/LuaExecutor.h"
 #import <UIKit/UIKit.h>
 
@@ -9,7 +12,7 @@ static XZXCore *sharedCoreInstance = nil;
 + (instancetype)shared {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        sharedCoreInstance = [[XZXCore alloc] init];
+        sharedCoreInstance = [[self alloc] init];
     });
     return sharedCoreInstance;
 }
@@ -18,58 +21,80 @@ static XZXCore *sharedCoreInstance = nil;
     self = [super init];
     if (self) {
         _isInGame = NO;
-        _editorWindow = nil;
-        NSLog(@"[XZX] Core instance created");
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self 
+                                                 selector:@selector(applicationDidBecomeActive) 
+                                                     name:UIApplicationDidBecomeActiveNotification 
+                                                   object:nil];
     }
     return self;
 }
 
++ (void)load {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), 
+                   dispatch_get_main_queue(), ^{
+        [[XZXCore shared] initialize];
+    });
+}
+
 - (void)initialize {
-    NSLog(@"[XZX] initialize called");
-    
-    @try {
-        InitLua();
-        NSLog(@"[XZX] Lua initialized");
-        
-        [self startGameStateMonitoring];
-        NSLog(@"[XZX] Monitoring started");
-        
-    } @catch (NSException *exception) {
-        NSLog(@"[XZX] CRASH: %@", exception);
-    }
+    [[XZXAntiDetection shared] initializeProtection];
+    [self startGameStateMonitoring];
+    hook_roblox_functions();
+    InitLua();
+}
+
+- (void)applicationDidBecomeActive {
+    [self startGameStateMonitoring];
 }
 
 - (void)startGameStateMonitoring {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
-        @autoreleasepool {
-            BOOL lastInGame = NO;
-            while (YES) {
-                @try {
-                    [NSThread sleepForTimeInterval:1.0];
-                    BOOL currentlyInGame = YES;
-                    
-                    if (currentlyInGame && !lastInGame) {
-                        dispatch_async(dispatch_get_main_queue(), ^{
-                            [self onGameJoined];
-                        });
-                    }
-                    lastInGame = currentlyInGame;
-                } @catch (NSException *e) {
-                    NSLog(@"[XZX] Monitor error: %@", e);
-                }
+        while (YES) {
+            BOOL currentlyInGame = [self checkIfInGame];
+            
+            if (currentlyInGame && !self.isInGame) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [self onGameJoined];
+                });
+            } else if (!currentlyInGame && self.isInGame) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [self onGameLeft];
+                });
             }
+            
+            [NSThread sleepForTimeInterval:1.0];
         }
     });
 }
 
+- (BOOL)checkIfInGame {
+    return isPlayerInGame();
+}
+
 - (void)onGameJoined {
     self.isInGame = YES;
-    NSLog(@"[XZX] Game joined");
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (!self.editorWindow) {
+            MainViewController *vc = [[MainViewController alloc] init];
+            self.editorWindow = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+            self.editorWindow.windowLevel = UIWindowLevelAlert + 1;
+            self.editorWindow.rootViewController = vc;
+            self.editorWindow.backgroundColor = [UIColor clearColor];
+        }
+        
+        self.editorWindow.hidden = NO;
+        [self.editorWindow makeKeyAndVisible];
+    });
 }
 
 - (void)onGameLeft {
     self.isInGame = NO;
-    NSLog(@"[XZX] Game left");
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.editorWindow.hidden = YES;
+    });
 }
 
 void notify_game_joined(void) {
@@ -78,6 +103,11 @@ void notify_game_joined(void) {
 
 void notify_game_left(void) {
     [[XZXCore shared] onGameLeft];
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [super dealloc];
 }
 
 @end

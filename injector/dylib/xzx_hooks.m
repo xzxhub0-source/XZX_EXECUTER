f36fe6a2168558ed931562bfcf53f8c938b3e770
xzxhub0-source <xzxhub0@gmail.com>
2026-03-14 16:09:22 -0400

    Update xzx_hooks.m

--- injector/dylib/xzx_hooks.m
+++ injector/dylib/xzx_hooks.m
@@ -1,150 +1,71 @@
 #import "xzx_hooks.h"
-#import <mach/mach.h>
-#import <mach-o/dyld.h>
 #import <objc/runtime.h>
 #import <objc/message.h>
+#import <Foundation/Foundation.h>
 
-static void *original_functions[256];
-static int hook_count = 0;
-static uintptr_t roblox_base = 0;
+static BOOL isPlayerInGameState = NO;
+static NSTimer *gameStateTimer = nil;
 
-// Fishhook-style symbol rebinding - SAFE method
-struct rebinding {
-    const char *name;
-    void *replacement;
-    void **replaced;
-};
-
-static int rebind_symbols(struct rebinding rebindings[], size_t rebindings_nel) {
-    // This uses dyld's dynamic symbol lookup instead of direct memory patching
-    for (size_t i = 0; i < rebindings_nel; i++) {
-        void *symbol = dlsym(RTLD_DEFAULT, rebindings[i].name);
-        if (symbol) {
-            *rebindings[i].replaced = symbol;
-            
-            // Use dyld API to safely rebind - this doesn't trigger code signing
-            // In practice, you'd use fishhook or similar
-        }
-    }
-    return 0;
-}
-
-// Objective-C method swizzling - SAFE method
-void swizzleMethod(Class class, SEL originalSelector, SEL swizzledSelector) {
-    Method originalMethod = class_getInstanceMethod(class, originalSelector);
-    Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-    
-    BOOL didAddMethod = class_addMethod(class,
-                                        originalSelector,
-                                        method_getImplementation(swizzledMethod),
-                                        method_getTypeEncoding(swizzledMethod));
-    
-    if (didAddMethod) {
-        class_replaceMethod(class,
-                            swizzledSelector,
-                            method_getImplementation(originalMethod),
-                            method_getTypeEncoding(originalMethod));
-    } else {
-        method_exchangeImplementations(originalMethod, swizzledMethod);
-    }
-}
-
-// Function hooking using MSHookFunction-style but with safer implementation
-void install_hook(void *target, void *replacement) {
-    if (!target || !replacement) return;
-    
-    // Don't use vm_protect - that's what's causing the crash
-    // Instead, use safer alternatives:
-    
-    // 1. For Objective-C methods, use method swizzling
-    // 2. For C functions, use symbol rebinding via fishhook
-    // 3. For C++ vtables, use virtual table patching (still risky)
-    
-    // Save original for later restoration
-    original_functions[hook_count++] = target;
-    
-    // Use safer method - create a trampoline in executable memory
-    // that's properly allocated with PROT_EXEC
-    void *trampoline = mmap(NULL, 128,
-                            PROT_READ | PROT_WRITE | PROT_EXEC,
-                            MAP_ANON | MAP_PRIVATE,
-                            -1, 0);
-    
-    if (trampoline != MAP_FAILED) {
-        // Generate a trampoline that jumps to replacement
-        // This is safer than modifying existing code pages
-        uint32_t *code = (uint32_t *)trampoline;
-        
-        // ARM64 trampoline:
-        // LDR X17, #8
-        // BR X17
-        // [address]
-        code[0] = 0x58000051;
-        code[1] = 0xD61F0220;
-        *(uint64_t *)&code[2] = (uint64_t)replacement;
-        
-        __builtin___clear_cache((char *)trampoline, (char *)trampoline + 128);
-        
-        // Store trampoline address
-        // This is what you'd use as the hooked function
-    }
-}
-
-// Safe hook for Roblox functions
 void hook_roblox_functions(void) {
-    uint32_t count = _dyld_image_count();
+    // DO NOT use vm_protect or any memory modification
+    // DO NOT attempt to hook C functions directly
     
-    for (uint32_t i = 0; i < count; i++) {
-        const char *name = _dyld_get_image_name(i);
-        if (strstr(name, "Roblox")) {
-            roblox_base = (uintptr_t)_dyld_get_image_header(i);
-            break;
-        }
-    }
+    // Use ONLY Objective-C runtime swizzling - this is SAFE
     
-    // Instead of memory patching, use Objective-C swizzling
-    // for Roblox's Objective-C classes
+    Class dataModelClass = NSClassFromString(@"RobloxDataModel");
+    if (!dataModelClass) return;
     
-    Class taskSchedulerClass = NSClassFromString(@"RBXTaskScheduler");
-    if (taskSchedulerClass) {
-        SEL originalSel = NSSelectorFromString(@"runTask:");
-        SEL swizzledSel = NSSelectorFromString(@"xzx_runTask:");
+    SEL originalSel = NSSelectorFromString(@"placeId");
+    SEL swizzledSel = NSSelectorFromString(@"xzx_placeId");
+    
+    Method originalMethod = class_getInstanceMethod(dataModelClass, originalSel);
+    if (!originalMethod) return;
+    
+    IMP swizzledImp = imp_implementationWithBlock(^id(id self) {
+        isPlayerInGameState = YES;
         
-        // Add our swizzled method
-        IMP swizzledImp = imp_implementationWithBlock(^(id self, id task) {
-            // Call our hook first
-            notify_game_joined();
-            
-            // Call original
-            struct objc_super super = {
-                .receiver = self,
-                .super_class = class_getSuperclass(taskSchedulerClass)
-            };
-            ((void (*)(struct objc_super *, SEL, id))objc_msgSendSuper)(&super, originalSel, task);
-        });
-        
-        class_addMethod(taskSchedulerClass, swizzledSel, swizzledImp, "v@:@");
-        
-        // Swizzle
-        Method originalMethod = class_getInstanceMethod(taskSchedulerClass, originalSel);
-        Method swizzledMethod = class_getInstanceMethod(taskSchedulerClass, swizzledSel);
+        struct objc_super super = {
+            .receiver = self,
+            .super_class = class_getSuperclass(dataModelClass)
+        };
+        return ((id(*)(struct objc_super*, SEL))objc_msgSendSuper)(&super, originalSel);
+    });
+    
+    BOOL added = class_addMethod(dataModelClass, swizzledSel, swizzledImp, method_getTypeEncoding(originalMethod));
+    
+    if (added) {
+        Method swizzledMethod = class_getInstanceMethod(dataModelClass, swizzledSel);
         method_exchangeImplementations(originalMethod, swizzledMethod);
     }
 }
 
 void unhook_roblox_functions(void) {
-    hook_count = 0;
+    // No need to unhook - let it be
 }
 
-bool isPlayerInGame(void) {
-    // Use Objective-C runtime to check game state safely
-    Class dataModelClass = NSClassFromString(@"RobloxDataModel");
-    if (dataModelClass) {
-        id dataModel = [dataModelClass performSelector:NSSelectorFromString(@"sharedDataModel")];
-        if (dataModel) {
-            id placeId = [dataModel performSelector:NSSelectorFromString(@"placeId")];
-            return placeId != nil;
+BOOL isPlayerInGame(void) {
+    return isPlayerInGameState;
+}
+
+// Safe way to check game state without memory hooks
+void startGameStatePolling(void) {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        while (YES) {
+            @autoreleasepool {
+                Class dataModelClass = NSClassFromString(@"RobloxDataModel");
+                if (dataModelClass) {
+                    id dataModel = [dataModelClass performSelector:NSSelectorFromString(@"sharedDataModel")];
+                    if (dataModel) {
+                        id placeId = [dataModel performSelector:NSSelectorFromString(@"placeId")];
+                        isPlayerInGameState = (placeId != nil);
+                    } else {
+                        isPlayerInGameState = NO;
+                    }
+                } else {
+                    isPlayerInGameState = NO;
+                }
+            }
+            [NSThread sleepForTimeInterval:1.0];
         }
-    }
-    return NO;
+    });
 }

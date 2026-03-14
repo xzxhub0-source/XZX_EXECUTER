496503920d997a24878c87e954f333d0111bd568
xzxhub0-source <xzxhub0@gmail.com>
2026-03-14 13:45:28 -0400

    Update xzx_functions.h

--- injector/dylib/xzx_functions.h
+++ injector/dylib/xzx_functions.h
@@ -3,35 +3,25 @@
 
 #import <Foundation/Foundation.h>
 
-// Forward declaration for Lua state
-typedef struct lua_State lua_State;
-
 @interface XZXFunctions : NSObject
 + (instancetype)shared;
 - (void)executeLuaScript:(NSString *)script;
-
-// FFlag functions
 - (void)setFFlag:(NSString *)name value:(NSString *)value;
 - (NSString *)getFFlag:(NSString *)name;
-
-// Advanced functions
 - (NSString *)base64Encode:(NSString *)input;
 - (NSString *)base64Decode:(NSString *)input;
 - (void)websocketConnect:(NSString *)url;
 - (void)websocketSend:(NSString *)message;
 - (void)websocketClose;
-
-// Roblox-specific
-- (id)getRenderProperty:(NSString *)property;
-- (id)getHiddenProperty:(NSString *)property;
-- (void)fireSignal:(NSString *)signal withArguments:(NSArray *)args;
-- (NSArray *)getConnections:(NSString *)signal;
-
-// Delta library compatibility
-- (void)deltaFunction:(NSString *)name args:(NSArray *)args;
+- (id)rnetNextPacket;
+- (id)rnetReadPacket:(id)packet;
+- (void)rnetSendPacket:(id)packet;
+- (void)rnetDesync:(int)amount;
+- (void)rnetReconnect;
 
 @property (nonatomic, strong) NSMutableDictionary *fflags;
 @property (nonatomic, strong) id websocket;
+@property (nonatomic, strong) NSMutableArray *packetQueue;
 @end
 
 #endif

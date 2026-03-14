828374d14820c8d3247dd7c36aefb7f2134e27b8
xzxhub0-source <xzxhub0@gmail.com>
2026-03-14 13:51:19 -0400

    Update xzx_memory_obfuscator.h

--- injector/dylib/xzx_memory_obfuscator.h
+++ injector/dylib/xzx_memory_obfuscator.h
@@ -15,7 +15,6 @@
 - (NSString *)decryptString:(NSString *)encrypted withKey:(int)key;
 - (NSString *)encryptString:(NSString *)plain withKey:(int)key;
 - (BOOL)verifyMemoryIntegrity;
-- (void)repairCorruptedSections;
 
 @property (nonatomic, assign) BOOL isObfuscated;
 @property (nonatomic, strong) NSMutableDictionary *stringTable;

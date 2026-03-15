f1020147dd278fed59bef474e0b785048ee2f3f2
xzxhub0-source <xzxhub0@gmail.com>
2026-03-15 07:50:42 -0400

    Update xzx_physics_bypass.m

--- injector/dylib/xzx_physics_bypass.m
+++ injector/dylib/xzx_physics_bypass.m
@@ -1,56 +1,26 @@
 #import "xzx_physics_bypass.h"
 
-static XZXPhysicsBypass *sharedPhysicsBypassInstance = nil;
-
 @implementation XZXPhysicsBypass
 
 + (instancetype)shared {
+    static XZXPhysicsBypass *instance = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        sharedPhysicsBypassInstance = [[self alloc] init];
+        instance = [[XZXPhysicsBypass alloc] init];
     });
-    return sharedPhysicsBypassInstance;
+    return instance;
 }
 
-- (instancetype)init {
-    self = [super init];
-    if (self) {
-        _lastTeleportTime = 0;
-        _lastValidPosition = CGPointMake(0, 0);
-        _movementHistory = [NSMutableArray array];
-        _hasForceField = NO;
-    }
-    return self;
-}
-
-- (void)setTeleportGrace:(double)seconds {
-    _lastTeleportTime = [NSDate timeIntervalSinceReferenceDate];
-}
-
-- (void)respectForceField {
-    _hasForceField = YES;
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 5 * NSEC_PER_SEC), 
-                   dispatch_get_main_queue(), ^{
-        self.hasForceField = NO;
-    });
-}
-
+- (void)setTeleportGrace:(double)seconds {}
+- (void)respectForceField {}
 - (void)simulateServerAnchors {}
-
 - (void)validateWithRaycasts {}
-
 - (void)bypassSpeedDetection {}
-
 - (void)bypassFlyDetection {}
-
 - (void)bypassNoclipDetection {}
-
 - (void)bypassTeleportDetection {}
-
 - (void)gradualSpeedIncrease {}
-
 - (void)smoothTeleport {}
-
 - (void)simulatePhysicsLag {}
 
 @end

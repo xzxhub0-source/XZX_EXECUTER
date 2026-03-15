22943cf6d100c1697b131382394ac6f051d2bc30
xzxhub0-source <xzxhub0@gmail.com>
2026-03-15 07:49:38 -0400

    Update xzx_hook_manager.m

--- injector/dylib/xzx_hook_manager.m
+++ injector/dylib/xzx_hook_manager.m
@@ -1,98 +1,32 @@
 #import "xzx_hook_manager.h"
 
-static XZXHookManager *sharedHookManagerInstance = nil;
-
 @implementation XZXHookManager
 
 + (instancetype)shared {
+    static XZXHookManager *instance = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        sharedHookManagerInstance = [[self alloc] init];
+        instance = [[XZXHookManager alloc] init];
     });
-    return sharedHookManagerInstance;
+    return instance;
 }
 
-- (instancetype)init {
-    self = [super init];
-    if (self) {
-        _hooks = [NSMutableDictionary dictionary];
-        _originalFunctions = [NSMutableDictionary dictionary];
-        _exposedHooks = [NSMutableArray array];
-    }
-    return self;
-}
-
-- (void)initializeHookSystem {
-    return;
-}
-
-- (void)addHook:(NSString *)functionName original:(void *)original hook:(void *)hook {
-    if (functionName && hook) {
-        [_hooks setObject:[NSValue valueWithPointer:hook] forKey:functionName];
-        if (original) {
-            [_originalFunctions setObject:[NSValue valueWithPointer:original] forKey:functionName];
-        }
-    }
-}
-
-- (void)removeHook:(NSString *)functionName {
-    [_hooks removeObjectForKey:functionName];
-}
-
-- (void)restoreAllFunctions {
-    return;
-}
-
-- (BOOL)areHooksExposed {
-    return NO;
-}
-
-- (void)hideFromAdonis {
-    return;
-}
-
-- (void)spoofConnectionList {
-    return;
-}
-
-- (void)randomizeHookOrder {
-    return;
-}
-
-- (double)exposureRatio {
-    return 0.0;
-}
-
-- (void *)cloneFunction:(void *)original {
-    return original;
-}
-
-- (void)restoreFunction:(NSString *)functionName {
-    return;
-}
-
-- (NSString *)getFunctionHash:(void *)function {
-    return @"";
-}
-
-- (NSArray *)getConnections:(NSString *)signal {
-    return @[];
-}
-
-- (void)fireSignal:(NSString *)signal withArguments:(NSArray *)args {
-    return;
-}
-
-- (NSArray *)getSignalArguments:(NSString *)signal {
-    return @[];
-}
-
-- (BOOL)canSignalReplicate:(NSString *)signal {
-    return YES;
-}
-
-- (void)replicateSignal:(NSString *)signal withArguments:(NSArray *)args {
-    return;
-}
+- (void)initializeHookSystem {}
+- (void)addHook:(NSString *)functionName original:(void *)original hook:(void *)hook {}
+- (void)removeHook:(NSString *)functionName {}
+- (void)restoreAllFunctions {}
+- (BOOL)areHooksExposed { return NO; }
+- (void)hideFromAdonis {}
+- (void)spoofConnectionList {}
+- (void)randomizeHookOrder {}
+- (double)exposureRatio { return 0.0; }
+- (void *)cloneFunction:(void *)original { return original; }
+- (void)restoreFunction:(NSString *)functionName {}
+- (NSString *)getFunctionHash:(void *)function { return @""; }
+- (NSArray *)getConnections:(NSString *)signal { return @[]; }
+- (void)fireSignal:(NSString *)signal withArguments:(NSArray *)args {}
+- (NSArray *)getSignalArguments:(NSString *)signal { return @[]; }
+- (BOOL)canSignalReplicate:(NSString *)signal { return YES; }
+- (void)replicateSignal:(NSString *)signal withArguments:(NSArray *)args {}
 
 @end

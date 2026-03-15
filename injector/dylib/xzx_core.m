b1458a4e1eaa8a59da1607c6789730797e0848a6
xzxhub0-source <xzxhub0@gmail.com>
2026-03-15 07:28:40 -0400

    Update xzx_core.m

--- injector/dylib/xzx_core.m
+++ injector/dylib/xzx_core.m
@@ -1,7 +1,5 @@
 #import "xzx_core.h"
 #import "XZX-Swift.h"
-#import "xzx_hooks.h"
-#import "xzx_antidetection.h"
 #import "Core/LuaExecutor.h"
 #import <UIKit/UIKit.h>
 
@@ -12,7 +10,7 @@ static XZXCore *sharedCoreInstance = nil;
 + (instancetype)shared {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        sharedCoreInstance = [[self alloc] init];
+        sharedCoreInstance = [[XZXCore alloc] init];
     });
     return sharedCoreInstance;
 }
@@ -21,104 +19,82 @@ static XZXCore *sharedCoreInstance = nil;
     self = [super init];
     if (self) {
         _isInGame = NO;
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidBecomeActive) name:UIApplicationDidBecomeActiveNotification object:nil];
     }
     return self;
 }
 
 + (void)load {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-        [[XZXCore shared] initialize];
-    });
+    // DO NOTHING in load – it's too early and can trigger codesigning
 }
 
 - (void)initialize {
-    [[XZXAntiDetection shared] initializeProtection];
-    
-    // Start polling instead of hooking
-    [self startGameStatePolling];
-    
+    // Delay all initialization to avoid early execution
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 5 * NSEC_PER_SEC),
+                   dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+        [self delayedInit];
+    });
+}
+
+- (void)delayedInit {
     // Initialize Lua safely
     InitLua();
+    
+    // Start polling for game state (using only safe KVC, no swizzling)
+    [self startSafePolling];
 }
 
-- (void)applicationDidBecomeActive {
-    // Refresh state when app becomes active
-}
-
-- (void)startGameStatePolling {
+- (void)startSafePolling {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
         while (YES) {
             @autoreleasepool {
-                BOOL currentlyInGame = [self checkIfInGameSafely];
-                if (currentlyInGame && !self.isInGame) {
+                BOOL inGame = [self safeCheckInGame];
+                if (inGame && !self.isInGame) {
+                    self.isInGame = YES;
                     dispatch_async(dispatch_get_main_queue(), ^{
-                        [self onGameJoined];
+                        [self showUI];
                     });
-                } else if (!currentlyInGame && self.isInGame) {
+                } else if (!inGame && self.isInGame) {
+                    self.isInGame = NO;
                     dispatch_async(dispatch_get_main_queue(), ^{
-                        [self onGameLeft];
+                        [self hideUI];
                     });
                 }
-                self.isInGame = currentlyInGame;
             }
-            [NSThread sleepForTimeInterval:1.0];
+            [NSThread sleepForTimeInterval:2.0]; // Poll less frequently
         }
     });
 }
 
-- (BOOL)checkIfInGameSafely {
+- (BOOL)safeCheckInGame {
     @try {
+        // Use only KVC – no performSelector with unknown selectors
         Class dataModelClass = NSClassFromString(@"RobloxDataModel");
         if (!dataModelClass) return NO;
         
-        id dataModel = [dataModelClass performSelector:NSSelectorFromString(@"sharedDataModel")];
+        id dataModel = [dataModelClass valueForKey:@"sharedDataModel"];
         if (!dataModel) return NO;
         
-        id placeId = [dataModel performSelector:NSSelectorFromString(@"placeId")];
+        id placeId = [dataModel valueForKey:@"placeId"];
         return (placeId != nil);
-    } @catch (NSException *exception) {
+    } @catch (NSException *e) {
         return NO;
     }
 }
 
-- (BOOL)checkIfInGame {
-    return [self checkIfInGameSafely];
+- (void)showUI {
+    if (!self.editorWindow) {
+        MainViewController *vc = [[MainViewController alloc] init];
+        self.editorWindow = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+        self.editorWindow.windowLevel = UIWindowLevelAlert + 1;
+        self.editorWindow.rootViewController = vc;
+        self.editorWindow.backgroundColor = [UIColor clearColor];
+    }
+    self.editorWindow.hidden = NO;
+    [self.editorWindow makeKeyAndVisible];
 }
 
-- (void)onGameJoined {
-    self.isInGame = YES;
-    dispatch_async(dispatch_get_main_queue(), ^{
-        if (!self.editorWindow) {
-            MainViewController *vc = [[MainViewController alloc] init];
-            self.editorWindow = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-            self.editorWindow.windowLevel = UIWindowLevelAlert + 1;
-            self.editorWindow.rootViewController = vc;
-            self.editorWindow.backgroundColor = [UIColor clearColor];
-        }
-        self.editorWindow.hidden = NO;
-        [self.editorWindow makeKeyAndVisible];
-    });
-}
-
-- (void)onGameLeft {
-    self.isInGame = NO;
-    dispatch_async(dispatch_get_main_queue(), ^{
-        self.editorWindow.hidden = YES;
-    });
-}
-
-void notify_game_joined(void) {
-    [[XZXCore shared] onGameJoined];
-}
-
-void notify_game_left(void) {
-    [[XZXCore shared] onGameLeft];
-}
-
-- (void)dealloc {
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
-    [super dealloc];
+- (void)hideUI {
+    self.editorWindow.hidden = YES;
 }
 
 @end

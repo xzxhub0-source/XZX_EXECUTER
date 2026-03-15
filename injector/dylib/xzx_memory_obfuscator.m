d95efb3f3278fe487449d07b86a395daf4d41932
xzxhub0-source <xzxhub0@gmail.com>
2026-03-15 07:49:10 -0400

    Update xzx_memory_obfuscator.m

--- injector/dylib/xzx_memory_obfuscator.m
+++ injector/dylib/xzx_memory_obfuscator.m
@@ -1,80 +1,26 @@
 #import "xzx_memory_obfuscator.h"
 
-static XZXMemoryObfuscator *sharedMemoryObfuscatorInstance = nil;
-
 @implementation XZXMemoryObfuscator
 
 + (instancetype)shared {
+    static XZXMemoryObfuscator *instance = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        sharedMemoryObfuscatorInstance = [[self alloc] init];
+        instance = [[XZXMemoryObfuscator alloc] init];
     });
-    return sharedMemoryObfuscatorInstance;
+    return instance;
 }
 
-- (instancetype)init {
-    self = [super init];
-    if (self) {
-        _isObfuscated = NO;
-        _stringTable = [NSMutableDictionary dictionary];
-    }
-    return self;
-}
-
-- (void)obfuscateAllSections {
-    _isObfuscated = YES;
-}
-
-- (void)obfuscateDylibSections {
-    _isObfuscated = YES;
-}
-
-- (void)encryptStringTable {
-    return;
-}
-
-- (void)scrambleFunctionPointers {
-    return;
-}
-
-- (void)preventMemoryDumping {
-    return;
-}
-
-- (void)addDecoyFunctions {
-    return;
-}
-
-- (void)createHoneyPot {
-    return;
-}
-
-- (NSString *)decryptString:(NSString *)encrypted withKey:(int)key {
-    NSMutableString *decrypted = [NSMutableString string];
-    for (int i = 0; i < encrypted.length; i++) {
-        unichar c = [encrypted characterAtIndex:i];
-        c ^= key;
-        [decrypted appendFormat:@"%C", c];
-    }
-    return decrypted;
-}
-
-- (NSString *)encryptString:(NSString *)plain withKey:(int)key {
-    NSMutableString *encrypted = [NSMutableString string];
-    for (int i = 0; i < plain.length; i++) {
-        unichar c = [plain characterAtIndex:i];
-        c ^= key;
-        [encrypted appendFormat:@"%C", c];
-    }
-    return encrypted;
-}
-
-- (BOOL)verifyMemoryIntegrity {
-    return YES;
-}
-
-- (void)repairCorruptedSections {
-    return;
-}
+- (void)obfuscateAllSections {}
+- (void)obfuscateDylibSections {}
+- (void)encryptStringTable {}
+- (void)scrambleFunctionPointers {}
+- (void)preventMemoryDumping {}
+- (void)addDecoyFunctions {}
+- (void)createHoneyPot {}
+- (NSString *)decryptString:(NSString *)encrypted withKey:(int)key { return encrypted; }
+- (NSString *)encryptString:(NSString *)plain withKey:(int)key { return plain; }
+- (BOOL)verifyMemoryIntegrity { return YES; }
+- (void)repairCorruptedSections {}
 
 @end

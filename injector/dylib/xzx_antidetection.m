93dd91300b57e55654d6286a0863af660caf4ce1
xzxhub0-source <xzxhub0@gmail.com>
2026-03-15 07:41:29 -0400

    Update xzx_antidetection.m

--- injector/dylib/xzx_antidetection.m
+++ injector/dylib/xzx_antidetection.m
@@ -1,83 +1,29 @@
 #import "xzx_antidetection.h"
 
-static XZXAntiDetection *sharedAntiDetectionInstance = nil;
-
 @implementation XZXAntiDetection
 
 + (instancetype)shared {
+    static XZXAntiDetection *instance = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        sharedAntiDetectionInstance = [[self alloc] init];
+        instance = [[XZXAntiDetection alloc] init];
     });
-    return sharedAntiDetectionInstance;
+    return instance;
 }
 
-- (instancetype)init {
-    self = [super init];
-    if (self) {
-        _currentStrikes = 0;
-        _maxStrikes = 10;
-        _banProbability = 0.0;
-        _strikeReasons = [NSMutableDictionary dictionary];
-    }
-    return self;
-}
-
-- (void)initializeProtection {
-    return;
-}
-
-- (void)runIntegrityChecks {
-    return;
-}
-
-- (BOOL)isUnderInvestigation {
-    return NO;
-}
-
-- (void)emergencyShutdown {
-    return;
-}
-
-- (void)obfuscateMemory {
-    return;
-}
-
-- (void)randomizeInjectionPattern {
-    return;
-}
-
-- (void)spoofConnections {
-    return;
-}
-
-- (void)cleanTraces {
-    [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"XZXExecutionHistory"];
-    [[NSUserDefaults standardUserDefaults] synchronize];
-}
-
-- (void)bypassAdonis {
-    return;
-}
-
-- (void)bypassKRX {
-    return;
-}
-
-- (void)bypassSentinelAC {
-    return;
-}
-
-- (void)bypassPhysicsChecks {
-    return;
-}
-
-- (void)updateBanProbability {
-    _banProbability = (double)_currentStrikes / _maxStrikes;
-}
-
-- (BOOL)shouldSelfDestruct {
-    return _banProbability > 0.95;
-}
+- (void)initializeProtection {}
+- (void)runIntegrityChecks {}
+- (BOOL)isUnderInvestigation { return NO; }
+- (void)emergencyShutdown {}
+- (void)obfuscateMemory {}
+- (void)randomizeInjectionPattern {}
+- (void)spoofConnections {}
+- (void)cleanTraces {}
+- (void)bypassAdonis {}
+- (void)bypassKRX {}
+- (void)bypassSentinelAC {}
+- (void)bypassPhysicsChecks {}
+- (void)updateBanProbability {}
+- (BOOL)shouldSelfDestruct { return NO; }
 
 @end

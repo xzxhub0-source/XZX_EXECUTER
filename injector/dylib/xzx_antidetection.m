e433f3456b7aff34294bc2261dee8684e177a57c
xzxhub0-source <xzxhub0@gmail.com>
2026-03-15 08:27:01 -0400

    Update xzx_antidetection.m

--- injector/dylib/xzx_antidetection.m
+++ injector/dylib/xzx_antidetection.m
@@ -1,6 +1,18 @@
 #import "xzx_antidetection.h"
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+#import <dlfcn.h>
+#import <mach-o/dyld.h>
+#import <sys/sysctl.h>
 
 static XZXAntiDetection *sharedAntiDetectionInstance = nil;
+static char *obfuscation_key = "xzx_obfuscation_2026";
+
+@interface XZXAntiDetection ()
+@property (nonatomic, strong) NSMutableArray *detectionSignatures;
+@property (nonatomic, strong) NSMutableDictionary *runtimeStats;
+@property (nonatomic, assign) int injectionPhase;
+@end
 
 @implementation XZXAntiDetection
 
@@ -12,20 +24,255 @@ static XZXAntiDetection *sharedAntiDetectionInstance = nil;
     return sharedAntiDetectionInstance;
 }
 
-- (void)initializeProtection {
-    // DO NOTHING - any attempt to hide/modify memory causes crash
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _currentStrikes = 0;
+        _maxStrikes = 15;
+        _banProbability = 0.0;
+        _detectionSignatures = [NSMutableArray array];
+        _runtimeStats = [NSMutableDictionary dictionary];
+        _injectionPhase = 0;
+    }
+    return self;
 }
 
-- (BOOL)isUnderInvestigation { return NO; }
-- (void)emergencyShutdown {}
-- (void)obfuscateMemory {}
-- (void)randomizeInjectionPattern {}
-- (void)spoofConnections {}
-- (void)cleanTraces {}
-- (void)bypassAdonis {}
-- (void)bypassKRX {}
-- (void)bypassSentinelAC {}
-- (void)bypassPhysicsChecks {}
-- (BOOL)shouldSelfDestruct { return NO; }
+- (void)initializeProtection {
+    [self hideDylibFromDyld];
+    [self obfuscateMemoryRegions];
+    [self randomizeInjectionPatterns];
+    [self hookAntiDebug];
+    [self spoofSystemCalls];
+    [self cleanProcessTraces];
+    _injectionPhase = 1;
+}
+
+- (void)hideDylibFromDyld {
+    uint32_t count = _dyld_image_count();
+    for (uint32_t i = 0; i < count; i++) {
+        const char *name = _dyld_get_image_name(i);
+        if (strstr(name, "executor.dylib") || strstr(name, "xzx")) {
+            uintptr_t header = (uintptr_t)_dyld_get_image_header(i);
+            size_t size = 0;
+            
+            const uint8_t *cmd = (const uint8_t *)header + sizeof(struct mach_header_64);
+            for (int j = 0; j < ((struct mach_header_64 *)header)->ncmds; j++) {
+                struct load_command *lc = (struct load_command *)cmd;
+                if (lc->cmd == LC_SEGMENT_64) {
+                    struct segment_command_64 *seg = (struct segment_command_64 *)cmd;
+                    if (strcmp(seg->segname, "__TEXT") == 0) {
+                        size = seg->vmsize;
+                        break;
+                    }
+                }
+                cmd += lc->cmdsize;
+            }
+            
+            if (size > 0) {
+                vm_address_t addr = header;
+                vm_protect(mach_task_self(), addr & ~(PAGE_SIZE - 1), size + PAGE_SIZE, 0, 
+                           VM_PROT_READ | VM_PROT_WRITE | VM_PROT_EXECUTE);
+                
+                memset((void *)header, 0, 0x100);
+                
+                vm_protect(mach_task_self(), addr & ~(PAGE_SIZE - 1), size + PAGE_SIZE, 0, 
+                           VM_PROT_READ | VM_PROT_EXECUTE);
+            }
+            break;
+        }
+    }
+}
+
+- (void)obfuscateMemoryRegions {
+    vm_address_t regions[1024];
+    uint32_t count = 1024;
+    vm_region_submap_info_64 info;
+    mach_msg_type_number_t info_count = VM_REGION_SUBMAP_INFO_COUNT_64;
+    natural_t depth = 0;
+    vm_address_t address = 0;
+    
+    while (1) {
+        kern_return_t kr = vm_region_recurse_64(mach_task_self(), &address, &info_count, &info, &depth);
+        if (kr != KERN_SUCCESS) break;
+        
+        if (info.protection & VM_PROT_WRITE) {
+            uint8_t *buf = malloc(info.size);
+            memcpy(buf, (void *)address, info.size);
+            
+            for (int i = 0; i < info.size; i += 8) {
+                if (i + 8 <= info.size) {
+                    uint64_t *val = (uint64_t *)(buf + i);
+                    *val ^= 0xDEADBEEFDEADBEEF;
+                }
+            }
+            
+            memcpy((void *)address, buf, info.size);
+            free(buf);
+        }
+        
+        address += info.size;
+    }
+}
+
+- (void)randomizeInjectionPatterns {
+    struct timespec ts;
+    ts.tv_sec = 0;
+    ts.tv_nsec = (arc4random_uniform(500) + 100) * 1000000;
+    nanosleep(&ts, NULL);
+    
+    void *dummy = mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+    if (dummy != MAP_FAILED) {
+        memset(dummy, arc4random_uniform(256), 0x1000);
+        munmap(dummy, 0x1000);
+    }
+}
+
+- (void)hookAntiDebug {
+    void *handle = dlopen("/usr/lib/system/libsystem_kernel.dylib", RTLD_NOW);
+    if (handle) {
+        void *ptrace = dlsym(handle, "ptrace");
+        if (ptrace) {
+            vm_address_t addr = (vm_address_t)ptrace;
+            vm_protect(mach_task_self(), addr & ~(PAGE_SIZE - 1), PAGE_SIZE, 0, 
+                       VM_PROT_READ | VM_PROT_WRITE | VM_PROT_EXECUTE);
+            
+            uint32_t nop = 0xD503201F;
+            memcpy(ptrace, &nop, 4);
+            
+            vm_protect(mach_task_self(), addr & ~(PAGE_SIZE - 1), PAGE_SIZE, 0, 
+                       VM_PROT_READ | VM_PROT_EXECUTE);
+        }
+        dlclose(handle);
+    }
+}
+
+- (void)spoofSystemCalls {
+    int mib[4];
+    mib[0] = CTL_KERN;
+    mib[1] = KERN_PROC;
+    mib[2] = KERN_PROC_ALL;
+    mib[3] = 0;
+    
+    size_t size = 0;
+    sysctl(mib, 3, NULL, &size, NULL, 0);
+    
+    struct kinfo_proc *proc_list = malloc(size);
+    sysctl(mib, 3, proc_list, &size, NULL, 0);
+    
+    int proc_count = size / sizeof(struct kinfo_proc);
+    for (int i = 0; i < proc_count; i++) {
+        if (proc_list[i].kp_proc.p_pid == getpid()) {
+            proc_list[i].kp_proc.p_flag |= P_LNOATTACH;
+            break;
+        }
+    }
+    
+    free(proc_list);
+}
+
+- (void)cleanProcessTraces {
+    unsetenv("DYLD_INSERT_LIBRARIES");
+    unsetenv("CFN_USE_HRT");
+    unsetenv("DYLD_FORCE_FLAT_NAMESPACE");
+    
+    [[NSFileManager defaultManager] removeItemAtPath:@"/tmp/xzx_trace" error:nil];
+}
+
+- (void)runIntegrityChecks {
+    static int checkCounter = 0;
+    checkCounter++;
+    
+    if (checkCounter % 10 == 0) {
+        [self verifyMemoryIntegrity];
+    }
+    
+    if (checkCounter % 25 == 0) {
+        [self checkForDebugger];
+    }
+    
+    if (checkCounter % 50 == 0) {
+        [self updateBanProbability];
+    }
+}
+
+- (void)verifyMemoryIntegrity {
+    uint32_t count = _dyld_image_count();
+    for (uint32_t i = 0; i < count; i++) {
+        const char *name = _dyld_get_image_name(i);
+        if (strstr(name, "Roblox")) {
+            const struct mach_header *header = _dyld_get_image_header(i);
+            uint32_t magic = header->magic;
+            if (magic != MH_MAGIC_64) {
+                _currentStrikes += 2;
+            }
+            break;
+        }
+    }
+}
+
+- (void)checkForDebugger {
+    int mib[4];
+    struct kinfo_proc info;
+    size_t size = sizeof(info);
+    
+    mib[0] = CTL_KERN;
+    mib[1] = KERN_PROC;
+    mib[2] = KERN_PID;
+    mib[3] = getpid();
+    
+    if (sysctl(mib, 4, &info, &size, NULL, 0) == 0) {
+        if ((info.kp_proc.p_flag & P_TRACED) != 0) {
+            _currentStrikes += 5;
+            [self emergencyShutdown];
+        }
+    }
+}
+
+- (BOOL)isUnderInvestigation {
+    return _currentStrikes > (_maxStrikes / 2);
+}
+
+- (void)emergencyShutdown {
+    if ([self shouldSelfDestruct]) {
+        exit(0);
+    }
+}
+
+- (void)updateBanProbability {
+    _banProbability = (double)_currentStrikes / _maxStrikes;
+}
+
+- (BOOL)shouldSelfDestruct {
+    return _banProbability > 0.85;
+}
+
+- (void)bypassAdonis {
+    [self spoofSystemCalls];
+    [self cleanProcessTraces];
+}
+
+- (void)bypassKRX {
+    [self hookAntiDebug];
+    [self obfuscateMemoryRegions];
+}
+
+- (void)bypassSentinelAC {
+    [self hideDylibFromDyld];
+    [self randomizeInjectionPatterns];
+}
+
+- (void)bypassPhysicsChecks {
+    Class physicsClass = NSClassFromString(@"RBXPhysicsService");
+    if (physicsClass) {
+        SEL checkSel = NSSelectorFromString(@"checkPhysicsViolations:");
+        Method originalMethod = class_getClassMethod(physicsClass, checkSel);
+        if (originalMethod) {
+            IMP swizzledImp = imp_implementationWithBlock(^BOOL(id self, id violation) {
+                return NO;
+            });
+            method_setImplementation(originalMethod, swizzledImp);
+        }
+    }
+}
 
 @end

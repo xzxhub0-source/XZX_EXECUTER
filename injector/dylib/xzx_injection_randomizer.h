ab22f6775ffda1316b03c042902c826dd43abd92
xzxhub0-source <xzxhub0@gmail.com>
2026-04-08 19:02:02 -0400

    Update xzx_injection_randomizer.h

--- injector/dylib/xzx_injection_randomizer.h
+++ injector/dylib/xzx_injection_randomizer.h
@@ -1,70 +1,17 @@
-#import "xzx_hook_manager.h"
+#ifndef XZX_INJECTION_RANDOMIZER_H
+#define XZX_INJECTION_RANDOMIZER_H
 
-static XZXHookManager *sharedHookManagerInstance = nil;
+#import <Foundation/Foundation.h>
 
-@implementation XZXHookManager
-
-+ (instancetype)shared {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        sharedHookManagerInstance = [[self alloc] init];
-    });
-    return sharedHookManagerInstance;
-}
-
-- (instancetype)init {
-    self = [super init];
-    if (self) {
-        _hooks = [NSMutableDictionary dictionary];
-        _originalFunctions = [NSMutableDictionary dictionary];
-        _exposedHooks = [NSMutableArray array];
-    }
-    return self;
-}
-
-- (void)initializeHookSystem {}
-
-- (void)addHook:(NSString *)functionName original:(void *)original hook:(void *)hook {
-    if (functionName && hook) {
-        [_hooks setObject:[NSValue valueWithPointer:hook] forKey:functionName];
-        if (original) {
-            [_originalFunctions setObject:[NSValue valueWithPointer:original] forKey:functionName];
-        }
-    }
-}
-
-- (void)removeHook:(NSString *)functionName {
-    [_hooks removeObjectForKey:functionName];
-}
-
-- (void)restoreAllFunctions {}
-
-- (BOOL)areHooksExposed {
-    return NO;
-}
-
-- (void)hideFromAdonis {}
-
-- (void)spoofConnectionList {}
-
-- (void)randomizeHookOrder {}
-
-- (double)exposureRatio {
-    return 0.0;
-}
-
-- (void *)cloneFunction:(void *)original {
-    return NULL;
-}
-
-- (NSString *)getFunctionHash:(void *)function {
-    return @"";
-}
-
-- (NSArray *)getConnections:(NSString *)signal {
-    return @[];
-}
-
-- (void)fireSignal:(NSString *)signal withArguments:(NSArray *)args {}
-
+@interface XZXInjectionRandomizer : NSObject
++ (instancetype)shared;
+- (void)randomizeNextInjection;
+- (void)randomizeInjectionTiming;
+- (void)randomizeMemoryAllocation;
+- (void)randomizeThreadCreation;
+- (void)avoidSignaturePatterns;
+- (double)randomnessScore;
+- (BOOL)isPatternDetectable;
 @end
+
+#endif

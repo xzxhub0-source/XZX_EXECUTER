640effedca104d68e6f8de2be30898eeb80fdb2e
xzxhub0-source <xzxhub0@gmail.com>
2026-04-08 19:01:02 -0400

    Update xzx_hooks.m

--- injector/dylib/xzx_hooks.m
+++ injector/dylib/xzx_hooks.m
@@ -1,4 +1,5 @@
 #import "xzx_hooks.h"
+#import "xzx_core.h"
 #import <mach/mach.h>
 #import <mach-o/dyld.h>
 #import <objc/runtime.h>
@@ -9,72 +10,42 @@ static int method_count = 0;
 static uintptr_t roblox_base = 0;
 static BOOL hooks_active = NO;
 
+void notify_game_joined(void) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (![[XZXCore shared] isInGame]) {
+            [[XZXCore shared] showOverlay];
+        }
+    });
+}
+
 void install_hook(void *target, void *replacement, void **original) {
     if (!target || !replacement) return;
-    
-    if (original) {
-        *original = target;
-    }
-    
+    if (original) *original = target;
     original_methods[method_count++] = target;
-    
+
     vm_address_t address = (vm_address_t)target;
-    vm_protect(mach_task_self(), address & ~(PAGE_SIZE - 1), PAGE_SIZE, 0, 
+    vm_protect(mach_task_self(), address & ~(PAGE_SIZE - 1), PAGE_SIZE, 0,
                VM_PROT_READ | VM_PROT_WRITE | VM_PROT_EXECUTE);
-    
-    uint32_t branch_instruction = 0x14000000 | 
+
+    uint32_t branch = 0x14000000 |
         (((uint32_t)((uintptr_t)replacement - (uintptr_t)target) >> 2) & 0x03FFFFFF);
-    
-    memcpy(target, &branch_instruction, 4);
-    
+    memcpy(target, &branch, 4);
     __builtin___clear_cache((char *)target, (char *)target + 4);
-    
-    vm_protect(mach_task_self(), address & ~(PAGE_SIZE - 1), PAGE_SIZE, 0, 
+
+    vm_protect(mach_task_self(), address & ~(PAGE_SIZE - 1), PAGE_SIZE, 0,
                VM_PROT_READ | VM_PROT_EXECUTE);
 }
 
 void hook_roblox_functions(void) {
     if (hooks_active) return;
     hooks_active = YES;
-    
+
     uint32_t count = _dyld_image_count();
     for (uint32_t i = 0; i < count; i++) {
         const char *name = _dyld_get_image_name(i);
         if (strstr(name, "Roblox") || strstr(name, "RobloxPlayer")) {
             roblox_base = (uintptr_t)_dyld_get_image_header(i);
             break;
-        }
-    }
-    
-    if (!roblox_base) return;
-    
-    Class dataModelClass = NSClassFromString(@"RobloxDataModel");
-    if (dataModelClass) {
-        SEL sharedSel = NSSelectorFromString(@"sharedDataModel");
-        Method originalMethod = class_getClassMethod(dataModelClass, sharedSel);
-        if (originalMethod) {
-            IMP originalImp = method_getImplementation(originalMethod);
-            IMP hookImp = imp_implementationWithBlock(^id(void) {
-                notify_game_joined();
-                id (*originalFunc)(id, SEL) = (id(*)(id, SEL))originalImp;
-                return originalFunc(dataModelClass, sharedSel);
-            });
-            method_setImplementation(originalMethod, hookImp);
-        }
-    }
-    
-    Class workspaceClass = NSClassFromString(@"RobloxWorkspace");
-    if (workspaceClass) {
-        SEL getSel = NSSelectorFromString(@"sharedWorkspace");
-        Method originalMethod = class_getClassMethod(workspaceClass, getSel);
-        if (originalMethod) {
-            IMP originalImp = method_getImplementation(originalMethod);
-            IMP hookImp = imp_implementationWithBlock(^id(void) {
-                id (*originalFunc)(id, SEL) = (id(*)(id, SEL))originalImp;
-                id workspace = originalFunc(workspaceClass, getSel);
-                return workspace;
-            });
-            method_setImplementation(originalMethod, hookImp);
         }
     }
 }
@@ -85,28 +56,13 @@ void unhook_roblox_functions(void) {
 
 bool isPlayerInGame(void) {
     Class dataModelClass = NSClassFromString(@"RobloxDataModel");
-    if (!dataModelClass) return NO;
-    
+    if (!dataModelClass) return false;
     SEL sharedSel = NSSelectorFromString(@"sharedDataModel");
-    if (![dataModelClass respondsToSelector:sharedSel]) return NO;
-    
-    id dataModel = ((id(*)(id, SEL))objc_msgSend)(dataModelClass, sharedSel);
-    if (!dataModel) return NO;
-    
+    if (![dataModelClass respondsToSelector:sharedSel]) return false;
+    id dataModel = ((id(*)(id, SEL))objc_msgSend)((id)dataModelClass, sharedSel);
+    if (!dataModel) return false;
     SEL placeIdSel = NSSelectorFromString(@"placeId");
-    if (![dataModel respondsToSelector:placeIdSel]) return NO;
-    
+    if (![dataModel respondsToSelector:placeIdSel]) return false;
     id placeId = ((id(*)(id, SEL))objc_msgSend)(dataModel, placeIdSel);
-    return (placeId != nil);
-}
-
-uintptr_t get_datamodel_address(void) {
-    Class dataModelClass = NSClassFromString(@"RobloxDataModel");
-    if (!dataModelClass) return 0;
-    
-    SEL sharedSel = NSSelectorFromString(@"sharedDataModel");
-    if (![dataModelClass respondsToSelector:sharedSel]) return 0;
-    
-    id dataModel = ((id(*)(id, SEL))objc_msgSend)(dataModelClass, sharedSel);
-    return (uintptr_t)dataModel;
+    return placeId && [placeId intValue] != 0;
 }

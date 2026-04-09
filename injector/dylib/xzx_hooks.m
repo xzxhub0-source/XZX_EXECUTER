4bd03a2cabf661f4636a05b41c5832f3def51a1e
xzxhub0-source <xzxhub0@gmail.com>
2026-04-09 05:20:21 -0400

    Update xzx_hooks.m

--- injector/dylib/xzx_hooks.m
+++ injector/dylib/xzx_hooks.m
@@ -10,9 +10,6 @@ static int method_count = 0;
 static uintptr_t roblox_base = 0;
 static BOOL hooks_active = NO;
 
-// FIX: was calling showOverlay without setting inGame = YES, so the monitor
-// loop could never transition to "left game", and showOverlay's new guard
-// would block it entirely.
 void notify_game_joined(void) {
     dispatch_async(dispatch_get_main_queue(), ^{
         XZXCore *core = [XZXCore shared];
@@ -57,19 +54,40 @@ void hook_roblox_functions(void) {
 void unhook_roblox_functions(void) { hooks_active = NO; }
 
 bool isPlayerInGame(void) {
-    Class dataModelClass = NSClassFromString(@"RBXDataModel");
-    if (!dataModelClass) dataModelClass = NSClassFromString(@"RobloxDataModel");
+    NSArray *classNames = @[
+        @"RBXDataModel",
+        @"RobloxDataModel",
+        @"DataModel",
+        @"RBXGame",
+        @"RobloxGame"
+    ];
+
+    Class dataModelClass = nil;
+    for (NSString *name in classNames) {
+        dataModelClass = NSClassFromString(name);
+        if (dataModelClass) break;
+    }
     if (!dataModelClass) return false;
 
-    SEL sharedSel = NSSelectorFromString(@"sharedDataModel");
-    if (![dataModelClass respondsToSelector:sharedSel]) return false;
-
-    id dataModel = ((id(*)(id, SEL))objc_msgSend)((id)dataModelClass, sharedSel);
+    NSArray *sharedSelNames = @[@"sharedDataModel", @"shared", @"singleton", @"instance"];
+    id dataModel = nil;
+    for (NSString *selName in sharedSelNames) {
+        SEL sel = NSSelectorFromString(selName);
+        if ([dataModelClass respondsToSelector:sel]) {
+            dataModel = ((id(*)(id, SEL))objc_msgSend)((id)dataModelClass, sel);
+            if (dataModel) break;
+        }
+    }
     if (!dataModel) return false;
 
-    SEL placeIdSel = NSSelectorFromString(@"placeId");
-    if (![dataModel respondsToSelector:placeIdSel]) return false;
+    NSArray *placeSelNames = @[@"placeId", @"PlaceId", @"currentPlaceId", @"gameId"];
+    for (NSString *selName in placeSelNames) {
+        SEL sel = NSSelectorFromString(selName);
+        if ([dataModel respondsToSelector:sel]) {
+            id placeId = ((id(*)(id, SEL))objc_msgSend)(dataModel, sel);
+            if (placeId && [placeId intValue] != 0) return true;
+        }
+    }
 
-    id placeId = ((id(*)(id, SEL))objc_msgSend)(dataModel, placeIdSel);
-    return placeId && [placeId intValue] != 0;
+    return false;
 }

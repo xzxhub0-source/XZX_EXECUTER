0d9d967be3c9e10594de76fc6df1151644c0289b
xzxhub0-source <xzxhub0@gmail.com>
2026-04-10 03:54:54 -0400

    Update xzx_core.h

--- injector/dylib/xzx_core.h
+++ injector/dylib/xzx_core.h
@@ -1,24 +1,21 @@
 #ifndef XZX_CORE_H
 #define XZX_CORE_H
-
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
 @interface XZXCore : NSObject
-
 + (instancetype)shared;
 - (void)initialize;
 - (void)showOverlay;
 - (void)hideOverlay;
 - (BOOL)isOverlayVisible;
-- (void)startGameMonitoring;
 - (BOOL)isInGame;
-
+- (BOOL)scanForMetalLayer:(UIView *)view;
+- (BOOL)viewHasMetalLayer:(UIView *)view;
+- (BOOL)viewHasWebView:(UIView *)view;
 @property (nonatomic, strong) UIWindow *overlayWindow;
 @property (nonatomic, assign) BOOL isInitialized;
 @property (nonatomic, assign) BOOL inGame;
-@property (nonatomic, assign) BOOL overlayAllowed;  // NEW: strict gate
-
 @end
 
 #endif

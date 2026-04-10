c72f2333063be957c71a96a121a0b096fa714bd1
xzxhub0-source <xzxhub0@gmail.com>
2026-04-10 16:05:30 -0400

    Update xzx_core.h

--- injector/dylib/xzx_core.h
+++ injector/dylib/xzx_core.h
@@ -2,22 +2,14 @@
 #define XZX_CORE_H
 
 #import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
 
 @interface XZXCore : NSObject
-
 + (instancetype)shared;
 - (void)initialize;
-- (void)showOverlay;      // called when game detected
-- (void)hideOverlay;      // called when leaving game
-- (BOOL)isOverlayVisible;
-- (BOOL)isInGame;
-
-@property (nonatomic, strong) UIWindow *overlayWindow;
-@property (nonatomic, strong) UIViewController *overlayViewController;
+- (void)showOverlay;   // now creates in-game UI
+- (void)hideOverlay;   // destroys it
 @property (nonatomic, assign) BOOL isInitialized;
 @property (nonatomic, assign) BOOL inGame;
-
 @end
 
 #endif

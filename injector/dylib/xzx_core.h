6d74d815750e152ebf7913a4d5096749055ba40a
xzxhub0-source <xzxhub0@gmail.com>
2026-04-10 05:34:33 -0400

    Update xzx_core.h

--- injector/dylib/xzx_core.h
+++ injector/dylib/xzx_core.h
@@ -10,10 +10,13 @@
 - (void)initialize;
 - (void)showOverlay;
 - (void)hideOverlay;
+- (void)resizeOverlayToNormal;  // NEW: resize from tiny to normal
 - (BOOL)isOverlayVisible;
 - (BOOL)isInGame;
+- (BOOL)viewHasMetalLayer:(UIView *)view;
 
 @property (nonatomic, strong) UIWindow *overlayWindow;
+@property (nonatomic, strong) UIViewController *overlayViewController;  // NEW: store reference
 @property (nonatomic, assign) BOOL isInitialized;
 @property (nonatomic, assign) BOOL inGame;
 

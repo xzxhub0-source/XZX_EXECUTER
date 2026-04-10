a40ab2a65671133e37190d57b0cc298df1a3eab0
xzxhub0-source <xzxhub0@gmail.com>
2026-04-10 16:03:50 -0400

    Update xzx_hooks.h

--- injector/dylib/xzx_hooks.h
+++ injector/dylib/xzx_hooks.h
@@ -2,11 +2,14 @@
 #define XZX_HOOKS_H
 
 #import <Foundation/Foundation.h>
+#include <lua.h>
 
 void hook_roblox_functions(void);
 void unhook_roblox_functions(void);
 void install_hook(void *target, void *replacement, void **original);
 void notify_game_joined(void);
 bool isPlayerInGame(void);
+lua_State* getRobloxLuaState(void);   // NEW
+void setupRemoteEventBridge(void);    // NEW
 
 #endif

210c43a335ec9517cf8367634bf37c1676d9e042
xzxhub0-source <xzxhub0@gmail.com>
2026-04-12 17:48:35 +0000

    Delete injector/dylib directory

--- injector/dylib/entry.m
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "xzx_core.h"
-__attribute__((constructor))
-static void xzx_entry(void) {
-    dispatch_async(dispatch_get_main_queue(), ^{ [[XZXCore shared] xzxStart]; });
-}

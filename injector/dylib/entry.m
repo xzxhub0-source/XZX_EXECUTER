079c4fed5bfd717949a6477a00546359f2ce422c
xzxhub0-source <xzxhub0@gmail.com>
2026-04-12 14:18:08 -0400

    Update entry.m

--- injector/dylib/entry.m
+++ injector/dylib/entry.m
@@ -1,8 +1,18 @@
 #import <UIKit/UIKit.h>
 #import "SRFXCore.h"
+
+static uint32_t random_delay(void) {
+    uint32_t base = 3.0 * NSEC_PER_SEC;
+    uint32_t jitter = arc4random_uniform(4 * NSEC_PER_SEC);
+    return base + jitter;
+}
+
 __attribute__((constructor))
-static void initialize() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3.0 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-        [[SRFXCore shared] start];
+static void initialize(void) {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, random_delay()),
+                   dispatch_get_main_queue(), ^{
+        if (@available(iOS 15.0, *)) {
+            [[SRFXCore shared] start];
+        }
     });
 }

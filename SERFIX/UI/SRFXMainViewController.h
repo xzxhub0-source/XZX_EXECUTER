cd3f63126d14af3746a6e97783d47d12893952cd
xzxhub0-source <xzxhub0@gmail.com>
2026-04-12 14:27:10 -0400

    Update SRFXMainViewController.h

--- SERFIX/UI/SRFXMainViewController.h
+++ SERFIX/UI/SRFXMainViewController.h
@@ -1,8 +1,13 @@
 #import <UIKit/UIKit.h>
+
 @interface SRFXMainViewController : UIViewController
+
 @property (nonatomic, strong) UITextView *editor;
 @property (nonatomic, strong) UITableView *scriptList;
 @property (nonatomic, strong) NSMutableArray *scripts;
 @property (nonatomic, strong) UIView *tabBar;
+@property (nonatomic, strong) UITextView *consoleView;
+@property (nonatomic, strong) NSMutableString *consoleText;
 @property (nonatomic, assign) NSInteger selectedTab;
+
 @end

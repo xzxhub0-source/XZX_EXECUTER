050e8775077a646fc715fd6d6933247d577d2225
xzxhub0-source <xzxhub0@gmail.com>
2026-04-12 14:52:01 -0400

    Update srfx_security.m

--- injector/dylib/srfx_security.m
+++ injector/dylib/srfx_security.m
@@ -1,5 +1,4 @@
 #import "SRFXSecurity.h"
-#import <sys/sysctl.h>
 #import <CommonCrypto/CommonDigest.h>
 
 @implementation SRFXSecurity
@@ -23,31 +22,10 @@
 }
 
 + (void)installAntiDebug {
-    int name[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid() };
-    struct kinfo_proc info;
-    size_t info_size = sizeof(info);
-    if (sysctl(name, 4, &info, &info_size, NULL, 0) != -1) {
-        if ((info.kp_proc.p_flag & P_TRACED) != 0) {
-            exit(0);
-        }
-    }
-
-    dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER,
-                                                      0, 0,
-                                                      dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0));
-    dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC, 0);
-    dispatch_source_set_event_handler(timer, ^{
-        if ([self isDebuggerAttached]) exit(0);
-    });
-    dispatch_resume(timer);
 }
 
 + (BOOL)isDebuggerAttached {
-    int name[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid() };
-    struct kinfo_proc info;
-    size_t info_size = sizeof(info);
-    if (sysctl(name, 4, &info, &info_size, NULL, 0) == -1) return NO;
-    return (info.kp_proc.p_flag & P_TRACED) != 0;
+    return NO;
 }
 
 @end

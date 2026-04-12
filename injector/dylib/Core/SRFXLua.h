a8d79ad46ffc21d14834f35f52d921262cc4cce9
xzxhub0-source <xzxhub0@gmail.com>
2026-04-12 14:03:13 -0400

    Create  SRFXLua.h

---
+++ injector/dylib/Core/SRFXLua.h
@@ -0,0 +1,14 @@
+#ifndef SRFXLua_h
+#define SRFXLua_h
+struct lua_State;
+#ifdef __cplusplus
+extern "C" {
+#endif
+void SRFXLuaInit(void);
+void SRFXLuaExecute(const char *script);
+void SRFXLuaRegister(const char *name, int (*func)(struct lua_State*));
+struct lua_State* SRFXLuaState(void);
+#ifdef __cplusplus
+}
+#endif
+#endif

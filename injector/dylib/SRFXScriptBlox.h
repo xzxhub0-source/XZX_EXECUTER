f110175d8dde818bc06161ae592f05dfcdc87dcd
xzxhub0-source <xzxhub0@gmail.com>
2026-04-12 14:25:35 -0400

    Update SRFXScriptBlox.h

--- injector/dylib/SRFXScriptBlox.h
+++ injector/dylib/SRFXScriptBlox.h
@@ -1,5 +1,7 @@
 #import <Foundation/Foundation.h>
+
 @interface SRFXScriptBlox : NSObject
 + (void)fetchTrending:(void(^)(NSArray *))completion;
 + (void)fetchScript:(NSString *)slug completion:(void(^)(NSString *))completion;
++ (void)search:(NSString *)query completion:(void(^)(NSArray *))completion;
 @end

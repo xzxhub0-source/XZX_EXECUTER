d40acf01e2897dbb5ea3f69e4103946ce32288cd
xzxhub0-source <xzxhub0@gmail.com>
2026-04-12 10:16:29 -0400

    Update entry.m

--- injector/dylib/entry.m
+++ injector/dylib/entry.m
@@ -1,10 +1,6 @@
 #import <UIKit/UIKit.h>
 #import "xzx_core.h"
-
 __attribute__((constructor))
-static void entry() {
-    NSLog(@"[XZX] Dylib loaded");
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [[XZXCore shared] initialize];
-    });
+static void xzx_entry(void) {
+    dispatch_async(dispatch_get_main_queue(), ^{ [[XZXCore shared] xzxStart]; });
 }

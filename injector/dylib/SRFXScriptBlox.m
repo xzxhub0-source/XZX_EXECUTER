76864af6944724074e58954f83a57da816d4027a
xzxhub0-source <xzxhub0@gmail.com>
2026-04-12 19:22:41 -0400

    Update SRFXScriptBlox.m

--- injector/dylib/SRFXScriptBlox.m
+++ injector/dylib/SRFXScriptBlox.m
@@ -1,70 +1,56 @@
 #import "SRFXScriptBlox.h"
 
-static NSString * const kBaseURL = @"https://scriptblox.com/api";
-
 @implementation SRFXScriptBlox
 
 + (void)fetchTrending:(void(^)(NSArray *))completion {
-    NSString *urlStr = [NSString stringWithFormat:@"%@/scripts?sort=trending&limit=50", kBaseURL];
-    NSURL *url = [NSURL URLWithString:urlStr];
-
-    [[[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *resp, NSError *err) {
-        if (!data) { completion(@[]); return; }
-        NSError *jsonErr;
-        id json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonErr];
-        if (![json isKindOfClass:[NSDictionary class]]) { completion(@[]); return; }
-
-        NSArray *scripts = json[@"scripts"];
-        NSMutableArray *result = [NSMutableArray array];
-        for (NSDictionary *s in scripts) {
-            [result addObject:@{
-                @"title": s[@"title"] ?: @"",
-                @"author": s[@"author"] ?: @"",
-                @"game": s[@"game"] ?: @"",
-                @"slug": s[@"slug"] ?: @"",
-                @"verified": s[@"verified"] ?: @NO
-            }];
+    NSURL *url = [NSURL URLWithString:@"https://scriptblox.com/api/scripts?sort=trending&limit=50"];
+    [[[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData *d, NSURLResponse *r, NSError *e) {
+        if (d) {
+            id json = [NSJSONSerialization JSONObjectWithData:d options:0 error:nil];
+            if ([json isKindOfClass:[NSDictionary class]]) {
+                NSArray *scripts = json[@"scripts"];
+                NSMutableArray *res = [NSMutableArray array];
+                for (NSDictionary *s in scripts) {
+                    [res addObject:@{@"title": s[@"title"]?:@"", @"author": s[@"author"]?:@"", @"game": s[@"game"]?:@"", @"slug": s[@"slug"]?:@""}];
+                }
+                completion(res); return;
+            }
         }
-        completion(result);
+        completion(@[]);
     }] resume];
 }
 
 + (void)fetchScript:(NSString *)slug completion:(void(^)(NSString *))completion {
-    NSString *urlStr = [NSString stringWithFormat:@"%@/scripts/%@", kBaseURL, slug];
-    NSURL *url = [NSURL URLWithString:urlStr];
-
-    [[[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *resp, NSError *err) {
-        if (!data) { completion(@""); return; }
-        NSError *jsonErr;
-        id json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonErr];
-        if ([json isKindOfClass:[NSDictionary class]]) {
-            NSString *code = json[@"script"];
-            if (code) { completion(code); return; }
+    NSString *urlStr = [NSString stringWithFormat:@"https://scriptblox.com/api/scripts/%@", slug];
+    [[[NSURLSession sharedSession] dataTaskWithURL:[NSURL URLWithString:urlStr] completionHandler:^(NSData *d, NSURLResponse *r, NSError *e) {
+        if (d) {
+            id json = [NSJSONSerialization JSONObjectWithData:d options:0 error:nil];
+            if ([json isKindOfClass:[NSDictionary class]]) {
+                NSString *code = json[@"script"];
+                if (code) { completion(code); return; }
+            }
         }
         completion(@"");
     }] resume];
 }
 
 + (void)search:(NSString *)query completion:(void(^)(NSArray *))completion {
-    NSString *urlStr = [NSString stringWithFormat:@"%@/scripts?search=%@&limit=30",
-                        kBaseURL, [query stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]]];
+    NSString *urlStr = [NSString stringWithFormat:@"https://scriptblox.com/api/scripts?search=%@&limit=30",
+                        [query stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]]];
     NSURL *url = [NSURL URLWithString:urlStr];
-    [[[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *resp, NSError *err) {
-        if (!data) { completion(@[]); return; }
-        id json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-        if ([json isKindOfClass:[NSDictionary class]]) {
-            NSArray *scripts = json[@"scripts"];
-            NSMutableArray *result = [NSMutableArray array];
-            for (NSDictionary *s in scripts) {
-                [result addObject:@{
-                    @"title": s[@"title"] ?: @"",
-                    @"slug": s[@"slug"] ?: @""
-                }];
+    [[[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData *d, NSURLResponse *r, NSError *e) {
+        if (d) {
+            id json = [NSJSONSerialization JSONObjectWithData:d options:0 error:nil];
+            if ([json isKindOfClass:[NSDictionary class]]) {
+                NSArray *scripts = json[@"scripts"];
+                NSMutableArray *res = [NSMutableArray array];
+                for (NSDictionary *s in scripts) {
+                    [res addObject:@{@"title": s[@"title"]?:@"", @"slug": s[@"slug"]?:@""}];
+                }
+                completion(res); return;
             }
-            completion(result);
-        } else {
-            completion(@[]);
         }
+        completion(@[]);
     }] resume];
 }
 

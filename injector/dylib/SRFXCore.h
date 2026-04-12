cb84653afe0ebe66cc4ed56f409df7d0dda5fcc7
xzxhub0-source <xzxhub0@gmail.com>
2026-04-12 14:18:57 -0400

    Update SRFXCore.h

--- injector/dylib/SRFXCore.h
+++ injector/dylib/SRFXCore.h
@@ -1,9 +1,13 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+
 @interface SRFXCore : NSObject
 + (instancetype)shared;
 - (void)start;
 - (void)showUI;
 - (void)hideUI;
+
 @property (nonatomic, strong) UIWindow *uiWindow;
+@property (nonatomic, assign) BOOL inGame;
+@property (nonatomic, strong) NSTimer *checkTimer;
 @end

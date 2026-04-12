a2f88e254b028858acb66f423ea6584d904f67c1
xzxhub0-source <xzxhub0@gmail.com>
2026-04-12 14:20:24 -0400

    Update srfx_memory.h

--- injector/dylib/srfx_memory.h
+++ injector/dylib/srfx_memory.h
@@ -1,4 +1,7 @@
 #import <Foundation/Foundation.h>
+
 @interface SRFXMemory : NSObject
-+ (instancetype)shared;
++ (void)obfuscate;
++ (void)encryptPage:(void *)page size:(size_t)size;
++ (void)decryptPage:(void *)page size:(size_t)size;
 @end

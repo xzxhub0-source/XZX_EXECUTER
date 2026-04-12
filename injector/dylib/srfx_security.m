0e5db14b8f23ba5cd8927a590ef1a2e47b99e234
xzxhub0-source <xzxhub0@gmail.com>
2026-04-12 14:22:33 -0400

    Update srfx_security.m

--- injector/dylib/srfx_security.m
+++ injector/dylib/srfx_security.m
@@ -1,4 +1,53 @@
-#import "srfx_security.h"
+#import "SRFXSecurity.h"
+#import <sys/sysctl.h>
+#import <CommonCrypto/CommonDigest.h>
+
 @implementation SRFXSecurity
-+ (instancetype)shared { static id i; static dispatch_once_t t; dispatch_once(&t,^{ i=[[self alloc] init];}); return i; }
+
++ (NSString *)spoofedHWID {
+    NSMutableString *hwid = [NSMutableString string];
+    for (int i = 0; i < 32; i++) {
+        uint8_t r = arc4random_uniform(16);
+        [hwid appendFormat:@"%X", r];
+    }
+
+    const char *input = [hwid UTF8String];
+    unsigned char digest[CC_SHA256_DIGEST_LENGTH];
+    CC_SHA256(input, (CC_LONG)strlen(input), digest);
+
+    NSMutableString *output = [NSMutableString string];
+    for (int i = 0; i < CC_SHA256_DIGEST_LENGTH; i++) {
+        [output appendFormat:@"%02X", digest[i]];
+    }
+    return output;
+}
+
++ (void)installAntiDebug {
+    int name[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid() };
+    struct kinfo_proc info;
+    size_t info_size = sizeof(info);
+    if (sysctl(name, 4, &info, &info_size, NULL, 0) != -1) {
+        if ((info.kp_proc.p_flag & P_TRACED) != 0) {
+            exit(0);
+        }
+    }
+
+    dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER,
+                                                      0, 0,
+                                                      dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0));
+    dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC, 0);
+    dispatch_source_set_event_handler(timer, ^{
+        if ([self isDebuggerAttached]) exit(0);
+    });
+    dispatch_resume(timer);
+}
+
++ (BOOL)isDebuggerAttached {
+    int name[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid() };
+    struct kinfo_proc info;
+    size_t info_size = sizeof(info);
+    if (sysctl(name, 4, &info, &info_size, NULL, 0) == -1) return NO;
+    return (info.kp_proc.p_flag & P_TRACED) != 0;
+}
+
 @end

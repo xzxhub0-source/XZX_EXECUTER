eb9c79ebeae1d8318ae71effdbd853c4cde08fc9
xzxhub0-source <xzxhub0@gmail.com>
2026-04-12 10:15:09 -0400

    Update xzx_core.h

--- injector/dylib/xzx_core.h
+++ injector/dylib/xzx_core.h
@@ -1,12 +1,10 @@
 #ifndef XZX_CORE_H
 #define XZX_CORE_H
-
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-
 @interface XZXCore : NSObject
 + (instancetype)shared;
-- (void)initialize;
+- (void)xzxStart;
 - (void)showOverlay;
 - (void)hideOverlay;
 - (BOOL)isOverlayVisible;
@@ -14,5 +12,4 @@
 @property (nonatomic, assign) BOOL isInitialized;
 @property (nonatomic, assign) BOOL inGame;
 @end
-
 #endif

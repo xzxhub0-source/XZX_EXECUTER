e6cd8d0a584f68534504e0b2d93bdee7ef697d4b
xzxhub0-source <xzxhub0@gmail.com>
2026-04-13 16:58:53 -0400

    Update SRFXHooks.h

--- injector/dylib/SRFXHooks.h
+++ injector/dylib/SRFXHooks.h
@@ -1,6 +1,14 @@
+// SRFXHooks.h
 #ifndef SRFXHooks_h
 #define SRFXHooks_h
-
 void SRFXHooksInstall(void);
-
 #endif
+
+// SRFXHooks.m
+#import "SRFXHooks.h"
+#import <objc/runtime.h>
+#import <objc/message.h>
+
+void SRFXHooksInstall(void) {
+    // stub
+}

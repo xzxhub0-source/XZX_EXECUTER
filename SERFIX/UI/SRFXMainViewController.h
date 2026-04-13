9ba2df928968075fac2f92d31ec485a664db0b67
xzxhub0-source <xzxhub0@gmail.com>
2026-04-13 19:01:42 -0400

    Update SRFXMainViewController.h

--- SERFIX/UI/SRFXMainViewController.h
+++ SERFIX/UI/SRFXMainViewController.h
@@ -2,12 +2,14 @@
 
 @interface SRFXMainViewController : UIViewController
 
-@property (nonatomic, strong) UITextView *editor;
-@property (nonatomic, strong) UITableView *scriptList;
-@property (nonatomic, strong) NSMutableArray *scripts;
-@property (nonatomic, strong) UIView *tabBar;
-@property (nonatomic, strong) UITextView *consoleView;
+@property (nonatomic, strong) UITextView      *editor;
+@property (nonatomic, strong) UITableView     *scriptList;
+@property (nonatomic, strong) NSMutableArray  *scripts;
+@property (nonatomic, strong) UITextView      *consoleView;
 @property (nonatomic, strong) NSMutableString *consoleText;
-@property (nonatomic, assign) NSInteger selectedTab;
+@property (nonatomic, assign) NSInteger        selectedTab;
+
+// Floating drag button (shown when panel is hidden)
+@property (nonatomic, strong) UIButton        *floatBtn;
 
 @end

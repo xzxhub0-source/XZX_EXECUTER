99cbe2a4547d4c9ad8157055f25f9c205ea46b1c
xzxhub0-source <xzxhub0@gmail.com>
2026-04-12 20:27:48 -0400

    Update SRFXCore.m

--- injector/dylib/SRFXCore.m
+++ injector/dylib/SRFXCore.m
@@ -1,7 +1,6 @@
 #import "SRFXCore.h"
 #import "SRFXMainViewController.h"
-#import <objc/runtime.h>
-#import <objc/message.h>
+#import <QuartzCore/QuartzCore.h>
 #include "Core/SRFXLua.h"
 
 static SRFXCore *instance = nil;
@@ -18,8 +17,8 @@ static const NSInteger kDebounce = 3;
 - (instancetype)init {
     self = [super init];
     if (self) {
-        _inGame        = NO;
-        _uiWindow      = nil;
+        _inGame = NO;
+        _uiWindow = nil;
         _positiveCount = 0;
         _negativeCount = 0;
     }
@@ -39,59 +38,79 @@ static const NSInteger kDebounce = 3;
     self.checkTimer = [NSTimer scheduledTimerWithTimeInterval:1.2
                                                       repeats:YES
                                                         block:^(NSTimer *t) {
-        __strong typeof(self) self = weak;
-        if (!self) { [t invalidate]; return; }
-
-        BOOL nowInGame = [self isInsideGame];
-
-        if (nowInGame) { self.positiveCount++; self.negativeCount  = 0; }
-        else           { self.negativeCount++; self.positiveCount  = 0; }
-
-        if (!self.inGame && self.positiveCount >= kDebounce) {
-            self.inGame = YES;
-            self.positiveCount = 0;
-            [self showUI];
-        } else if (self.inGame && self.negativeCount >= kDebounce) {
-            self.inGame = NO;
-            self.negativeCount = 0;
-            [self hideUI];
-        }
+        dispatch_async(dispatch_get_main_queue(), ^{
+            __strong typeof(self) strong = weak;
+            if (!strong) { [t invalidate]; return; }
+            [strong pollGameState];
+        });
     }];
+}
+
+- (void)pollGameState {
+    BOOL nowInGame = [self isInsideGame];
+
+    if (nowInGame) { self.positiveCount++; self.negativeCount = 0; }
+    else           { self.negativeCount++; self.positiveCount = 0; }
+
+    if (!self.inGame && self.positiveCount >= kDebounce) {
+        self.inGame = YES;
+        self.positiveCount = 0;
+        [self showUI];
+    } else if (self.inGame && self.negativeCount >= kDebounce) {
+        self.inGame = NO;
+        self.negativeCount = 0;
+        [self hideUI];
+    }
 }
 
 - (BOOL)isInsideGame {
     @try {
-        NSArray *classNames = @[@"RBXDataModel", @"RobloxDataModel", @"DataModel"];
-        Class dmClass = nil;
-        for (NSString *n in classNames) {
-            dmClass = NSClassFromString(n);
-            if (dmClass) break;
-        }
-        if (!dmClass) return NO;
+        for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
+            if (![scene isKindOfClass:[UIWindowScene class]]) continue;
+            UIWindowScene *windowScene = (UIWindowScene *)scene;
+            if (windowScene.activationState != UISceneActivationStateForegroundActive) continue;
 
-        NSArray *sharedSels = @[@"sharedDataModel", @"shared", @"singleton"];
-        id dm = nil;
-        for (NSString *sn in sharedSels) {
-            SEL s = NSSelectorFromString(sn);
-            if ([dmClass respondsToSelector:s]) {
-                dm = ((id(*)(id,SEL))objc_msgSend)((id)dmClass, s);
-                if (dm) break;
-            }
-        }
-        if (!dm) return NO;
+            BOOL statusBarHidden = windowScene.statusBarManager.statusBarHidden;
+            BOOL hasMetal = NO;
+            NSInteger buttonCount = 0;
 
-        NSArray *placeSelNames = @[@"placeId", @"PlaceId", @"currentPlaceId"];
-        for (NSString *sn in placeSelNames) {
-            SEL s = NSSelectorFromString(sn);
-            if ([dm respondsToSelector:s]) {
-                id pid = ((id(*)(id,SEL))objc_msgSend)(dm, s);
-                return pid && [pid intValue] != 0;
+            for (UIWindow *window in windowScene.windows) {
+                if (window == self.uiWindow) continue;
+                if (!hasMetal) hasMetal = [self viewHasMetalLayer:window];
+                if (buttonCount < 20) buttonCount += [self countButtonsInView:window];
             }
+
+            if (hasMetal && statusBarHidden && buttonCount < 6) return YES;
         }
-    } @catch (NSException *e) {
-        NSLog(@"[SRFX] isInsideGame error: %@", e);
-    }
+    } @catch (NSException *e) {}
     return NO;
+}
+
+- (BOOL)viewHasMetalLayer:(UIView *)view {
+    @try {
+        if ([view.layer isKindOfClass:NSClassFromString(@"CAMetalLayer")]) return YES;
+        for (CALayer *sub in view.layer.sublayers ?: @[]) {
+            if ([sub isKindOfClass:NSClassFromString(@"CAMetalLayer")]) return YES;
+            for (CALayer *s2 in sub.sublayers ?: @[])
+                if ([s2 isKindOfClass:NSClassFromString(@"CAMetalLayer")]) return YES;
+        }
+        for (UIView *sub in view.subviews)
+            if ([self viewHasMetalLayer:sub]) return YES;
+    } @catch (NSException *e) {}
+    return NO;
+}
+
+- (NSInteger)countButtonsInView:(UIView *)view {
+    NSInteger n = 0;
+    @try {
+        if ([view isKindOfClass:[UIButton class]]) n++;
+        if (n >= 20) return n;
+        for (UIView *sub in view.subviews) {
+            n += [self countButtonsInView:sub];
+            if (n >= 20) return n;
+        }
+    } @catch (NSException *e) {}
+    return n;
 }
 
 - (void)showUI {
@@ -121,8 +140,8 @@ static const NSInteger kDebounce = 3;
         self.uiWindow = [[UIWindow alloc] initWithWindowScene:scene];
         self.uiWindow.windowLevel = UIWindowLevelAlert + 5;
         self.uiWindow.backgroundColor = UIColor.clearColor;
-        self.uiWindow.hidden = YES;
         self.uiWindow.rootViewController = vc;
+        self.uiWindow.hidden = YES;
     }
 
     self.uiWindow.hidden = NO;
@@ -134,10 +153,19 @@ static const NSInteger kDebounce = 3;
     self.uiWindow.hidden = YES;
     for (UIScene *s in UIApplication.sharedApplication.connectedScenes) {
         if ([s isKindOfClass:[UIWindowScene class]]) {
-            UIWindow *rw = ((UIWindowScene *)s).windows.firstObject;
-            if (rw && rw != self.uiWindow) { [rw makeKeyWindow]; break; }
+            for (UIWindow *w in ((UIWindowScene *)s).windows) {
+                if (w != self.uiWindow && !w.isKeyWindow) {
+                    [w makeKeyWindow];
+                    return;
+                }
+            }
         }
     }
+}
+
+- (void)dealloc {
+    [self.checkTimer invalidate];
+    self.checkTimer = nil;
 }
 
 @end

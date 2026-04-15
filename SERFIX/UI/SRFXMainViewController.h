3e5f4144e36f657c9cea2c211c86ed6b386160f1
xzxhub0-source <xzxhub0@gmail.com>
2026-04-15 06:05:12 -0400

    Reorder properties in SRFXMainViewController.h

--- SERFIX/UI/SRFXMainViewController.h
+++ SERFIX/UI/SRFXMainViewController.h
@@ -1,15 +1,11 @@
 #import <UIKit/UIKit.h>
 
 @interface SRFXMainViewController : UIViewController
-
-@property (nonatomic, strong) UITextView      *editor;
-@property (nonatomic, strong) UITableView     *scriptList;
-@property (nonatomic, strong) NSMutableArray  *scripts;
-@property (nonatomic, strong) UITextView      *consoleView;
+@property (nonatomic, strong) UITextView *editor;
+@property (nonatomic, strong) UITableView *scriptList;
+@property (nonatomic, strong) NSMutableArray *scripts;
+@property (nonatomic, strong) UITextView *consoleView;
 @property (nonatomic, strong) NSMutableString *consoleText;
-@property (nonatomic, assign) NSInteger        selectedTab;
-
-// Floating drag button (shown when panel is hidden)
-@property (nonatomic, strong) UIButton        *floatBtn;
-
+@property (nonatomic, assign) NSInteger selectedTab;
+@property (nonatomic, strong) UIButton *floatBtn;
 @end
